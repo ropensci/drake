21bcc8890242195b9a869d8606f60a0b6d7aaee9
C240390 <will.landau@lilly.com>
2017-02-27 15:00:10 -0500

    Tweak

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   if(file.exists(".RData")) 
     warning("Auto-saved workspace file '.RData' detected. ",
-      "This is bad for reproducible command. ",
+      "This is bad for reproducible code. ",
       "You can remove it with unlink('.RData').")
   invisible()
 }
