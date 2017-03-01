b2aa9e7b6f9b509058f0e9479835d51beffae56e
Will Landau <will.landau@lilly.com>
2017-03-01 14:58:11 -0500

    Add tests

---
+++ tests/testthat/test-other-features.R
@@ -0,0 +1,67 @@
+# library(testthat); library(devtools); load_all()
+context("other-features")
+source("utils.R")
+
+test_that("console", {
+  args = dbug()
+  expect_output(console("build", "myinput", args))
+  x50 = paste(rep(0:9, 5), collapse = "")
+  x51 = paste0(x50, 0)
+  o1 = capture.output(console("build", x50, args))
+  o2 = capture.output(console("build", x51, args))
+  expect_equal(nchar(o1), nchar(o2), 50)
+  dots = "\\.\\.\\.$"
+  expect_false(grepl(dots, o1))
+  expect_true(grepl(dots, o2))
+  dclean()
+})
+
+test_that("check_args() via check() and run()", {
+  dclean()
+  args = dbug()
+  y = data.frame(x = 1, y = 2)
+  expect_error(check(y, envir = args$envir))
+  expect_error(run(y, envir = args$envir))
+  y = data.frame(target = character(0), command = character(0))
+  expect_error(check(y, envir = args$envir))
+  expect_error(run(y, envir = args$envir))
+  expect_error(check(args$plan, targets = character(0), envir = args$envir))
+  expect_error(run(args$plan, targets = character(0), envir = args$envir))
+  dclean()
+})
+
+test_that("deprecation", {
+  dclean()
+  plan = data.frame(code = 1, output = "x")
+  expect_warning(run(plan, verbose = FALSE))
+  dclean()
+  expect_warning(make(plan, verbose = FALSE))
+  dclean()
+})
+
+test_that("examples are listed and written", {
+  dclean()
+  x = examples_drake()
+  expect_true(is.character(x) & length(x) > 0)
+  for(i in x){
+    expect_false(file.exists(i))
+    example_drake(i)
+    expect_true(file.exists(i))
+    expect_true(file.info(i)$isdir)
+    unlink(i, recursive = TRUE)
+  }
+  dclean()
+})
+
+test_that("targets can be partially specified", {
+  dclean()
+  args = dbug()
+  args$targets = "'intermediatefile.rds'"
+  testrun(args)
+  expect_true(file.exists("intermediatefile.rds"))
+  expect_error(readd(final))
+  args$targets = "final"
+  testrun(args)
+  expect_true(is.numeric(readd(final)))
+  dclean()
+})
