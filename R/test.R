736f8959c3ee3792d881cdbd9fe8f98b0d24495e
C240390 <will.landau@lilly.com>
2017-04-05 20:49:20 -0400

    Fix test

--- R/test.R
+++ R/test.R
@@ -3,9 +3,9 @@
 # numbers of jobs and mclapply vs Makefiles.
 
 testopts = function(){list(
-  jobs = 2, # needs to be 1 for mclapply on Windows
-  parallelism = "parLapply") # use for shipped tests
- # parallelism = "mclapply")
+  jobs = 1, # needs to be 1 for mclapply on Windows
+ # parallelism = "parLapply") # use for shipped tests
+  parallelism = "mclapply")
  # parallelism = "Makefile")
 } # drake has to already be installed for the Makefile stuff
 
