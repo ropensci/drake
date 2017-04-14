1f30e1fc06901aee04512f4c1325428db0693dcf
C240390 <will.landau@lilly.com>
2017-04-14 11:11:25 -0400

    Use require() rather than library()

--- R/config.R
+++ R/config.R
@@ -26,7 +26,7 @@ add_packages_to_prework = function(packages, prework){
   packages = c("methods", packages) %>% unique
   if(!length(packages)) return(prework)
   package_list = deparse(packages) %>% paste(collapse = "\n")
-  paste0("if(!R.utils::isPackageLoaded(\"", packages, "\")) library(",
+  paste0("if(!R.utils::isPackageLoaded(\"", packages, "\")) require(",
     packages, ")", sep = "") %>%
     c(prework)
 }
