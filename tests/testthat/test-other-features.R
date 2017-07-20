1e248227042de8a7a0eee994adfaeb7175ec902a
C240390 <will.landau@lilly.com>
2017-07-20 02:14:54 -0400

    Try to increase test coverage

--- tests/testthat/test-other-features.R
+++ tests/testthat/test-other-features.R
@@ -56,6 +56,8 @@ test_that(".onLoad() warns correctly and .onAttach() works", {
   expect_warning(drake:::.onLoad())
   unlink(f)
   set.seed(0)
+  for(i in 1:100)
+    expect_true(is.character(drake_tip()))
   for(i in 1:1000) # Expect no errors or warnings.
     expect_silent(suppressPackageStartupMessages(drake:::.onAttach()))
 })
