4d395ed761a46205049d17da1711f4eeed35537e
Jasper <jasper.clarkberg@cfpb.gov>
2017-08-08 11:20:26 -0400

    Don't del() build_times just before building a target

--- R/build.R
+++ R/build.R
@@ -2,7 +2,6 @@ build = function(target, hash_list, config){
   hashes = hash_list[[target]]
   config$cache$set(key = target, value = "in progress", 
     namespace = "progress")
-  config$cache$del(key = target, namespace = "build_times")
   imported = !(target %in% config$plan$target)
   console(imported = imported, target = target, config = config) 
   if(imported)
