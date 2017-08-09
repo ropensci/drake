3716c5acaa421565bf2d210f0477b14d7774e5d8
Alex Axthelm <AAxthelm@che.in.gov>
2017-08-09 09:47:50 -0400

    Add some failing tests around reproducibility
    I want to be able to get the same object when I build, so that I can get
    the same simulation results when necessary

---
+++ tests/testthat/test-reproducible.R
@@ -0,0 +1,68 @@
+context("reproducible")
+
+test_that("Objects are reproducible", {
+  dclean()
+
+  data <- plan(
+    x = runif(20),
+    y = runif(20),
+    z = rnorm(20)
+    )
+  make(data)
+  old_x <- x
+  old_y <- y
+  old_z <- z
+
+  # Oh no, I've accidentally deleted some data that needs to be reproducible
+  clean(x, y, z)
+  make(data)
+  expect_identical(x, old_x)
+  expect_identical(y, old_y)
+  expect_identical(z, old_z)
+
+  dclean()
+})
+
+test_that("Objects are distinct", {
+  dclean()
+
+  data <- plan(
+    x = runif(20)
+    )
+  data_exp <- expand(data, values = c("a", "b"))
+  make(data_exp)
+
+  expect_false(identical(x_a, x_b))
+
+  dclean()
+})
+
+test_that("Sequential objects reproduce correctly", {
+  dclean()
+
+  first_stage <- plan(
+    x = runif(20)
+    )
+  methods <- plan(
+    xbar = mean(..dataset..),  #nolint
+    y = sample(..dataset.., 5)  #nolint
+    )
+  second_stage <- analyses(
+    methods,
+    data = first_stage
+    )
+  make(rbind(first_stage, second_stage))
+  old_x <- x
+  old_xbax_x <- xbar_x
+  old_y_y <- y_x
+
+  # How do I keep deleting my data like this? So clumsy.
+  clean(x, xbar_x, y_x)
+
+  make(rbind(first_stage, second_stage))
+  expect_identical(old_x, x)
+  expect_identical(old_xbax_x, xbar_x)
+  expect_identical(old_y_y, y_x)
+
+  dclean()
+})
