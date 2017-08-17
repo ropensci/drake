b839575020bf35e3ff3fb9e80e030ce0c399e4fe
C240390 <will.landau@lilly.com>
2017-08-17 13:22:14 -0400

    Change a test

--- tests/testthat/test-testing.R
+++ tests/testthat/test-testing.R
@@ -1,7 +1,9 @@
+context("testing")
+
 test_that("testing configs are there", {
+  old_opt <- getOption("drake_test_opt")
   expect_true(is.list(test_opt()))
   expect_true(is.list(test_opts))
-  old_opt <- getOption("drake_test_opt")
   set_test_opt(old_opt)
   expect_true(!is.null(getOption("drake_test_opt")))
   options(drake_test_opt = old_opt)
