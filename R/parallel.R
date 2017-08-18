180bd1d6d81b385a989741726832ddedc3825944
Jasper <jasper.clarkberg@cfpb.gov>
2017-08-18 12:41:50 -0400

    Switch to putting message in parallel_stage

--- R/parallel.R
+++ R/parallel.R
@@ -9,12 +9,13 @@ run_parallel = function(config, worker){
 parallel_stage = function(graph_remaining_targets, worker,
   config){
   config = inventory(config)
+  remaining_targets = V(graph_remaining_targets) %>% names %>% intersect(config$targets)
   candidates = next_targets(graph_remaining_targets)
+  cat("checking hashes for build phase, ", length(remaining_targets), "/", length(config$targets), " targets left\n", sep = "")
   hash_list = hash_list(targets = candidates, config = config)
   build_these = Filter(candidates, f = function(target)
     should_build(target = target, hash_list = hash_list,
       config = config))
-  cat("\n")
   hash_list = hash_list[build_these]
   if(length(build_these))
     worker(targets = build_these, hash_list = hash_list, config = config)
