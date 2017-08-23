965d4dcc2e42e2566e3e6981b6d303eb118b51fb
Alex Axthelm <AAxthelm@che.in.gov>
2017-08-23 10:25:09 -0400

    Format `dbug` for lintr

--- R/debug.R
+++ R/debug.R
@@ -1,10 +1,12 @@
-dbug = function(clean = TRUE){
-  if(clean) dclean()
+dbug <- function(clean = TRUE){
+  if (clean){
+    dclean()
+  }
+  envir <- new.env(parent = globalenv()) # for shipped unit tests
+  # Note that `envir = globalenv()` needs to work here too.
+  # This is addressed in issue #58
 
-  envir = new.env(parent = globalenv()) # for shipped unit tests
-#  envir = globalenv() # needs to work here too
-
-  imports = c(
+  imports <- c(
     "f <- function(x) {g(x) + a}",
     "g <- function(y) {h(y) + b}",
     "h <- function(y) {i(y) + j(y)}",
@@ -12,26 +14,39 @@ dbug = function(clean = TRUE){
     "j <- function(x) {x+2 + c}",
     "a <- 15",
     "b <- 20",
-    "c <- 25")
-  for(import in imports)
+    "c <- 25"
+    )
+  for (import in imports){
     eval(parse(text = import), envir = envir)
+  }
 
   saveRDS(1:10, "input.rds") # small files are always rehashed
-#  set.seed(0); saveRDS(rnorm(100000), "input.rds") # test file rehashing
+  # TODO: implement as a test on non-CRAN systems:
+  # relates to issues #58, #53, and #4 (closed, but relevant discussion)
+  # example code: set.seed(0); saveRDS(rnorm(100000), "input.rds")
 
-  plan = plan(list = c(
-    "'intermediatefile.rds'" = 
-      "saveRDS(combined, \"intermediatefile.rds\")",
-    yourinput = "f(1+1)",
-    nextone = "myinput + g(7)",
-    combined = "nextone + yourinput",
-    myinput = "readRDS('input.rds')",
-    final = "readRDS('intermediatefile.rds')"
-  ))
-  config = build_config(plan, targets = plan$target, envir = envir, 
-    parallelism = "mclapply", jobs = 1, prepend = character(0),
-    verbose = FALSE, packages = character(0), prework = character(0), 
-    command = "make", args = character(0))
+  plan <- plan(list = c(
+      "'intermediatefile.rds'" =
+        "saveRDS(combined, \"intermediatefile.rds\")",
+      yourinput = "f(1+1)",
+      nextone = "myinput + g(7)",
+      combined = "nextone + yourinput",
+      myinput = "readRDS('input.rds')",
+      final = "readRDS('intermediatefile.rds')"
+      ))
+  config <- build_config(
+    plan = plan,
+    targets = plan$target,
+    envir = envir,
+    parallelism = "mclapply",
+    jobs = 1,
+    prepend = character(0),
+    verbose = FALSE,
+    packages = character(0),
+    prework = character(0),
+    command = "make",
+    args = character(0)
+    )
   config
 }
 
