a6f6f318a137ad34c107222b16521b63c2bfc89b
Alex Axthelm <AAxthelm@che.in.gov>
2017-08-23 10:01:26 -0400

    format `console` function for lintr

--- R/console.R
+++ R/console.R
@@ -1,12 +1,17 @@
-console = function(imported, target, config){
-  if(!config$verbose) return()
-  if(is.na(imported))
-    action = color("could not find", "darkorchid3")
-  else if(imported)
-    action = color("import", "dodgerblue3")
-  else
-    action = color("build", "forestgreen")
-  if(nchar(target) > 50) target = paste0(substr(target, 1, 47), "...")
+console <- function(imported, target, config){
+  if (!config$verbose){
+    return()
+  }
+  if (is.na(imported)){
+    action <- color("could not find", "darkorchid3")
+  } else if (imported){
+    action <- color("import", "dodgerblue3")
+  } else{
+    action <- color("build", "forestgreen")
+  }
+  if (nchar(target) > 50){
+    target <- paste0(substr(target, 1, 47), "...")
+  }
   cat(action, " ", target, "\n", sep = "")
 }
 
