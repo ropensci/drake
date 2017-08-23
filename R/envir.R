fe23cebb466261cbd8cd2f97befd8269b15b2de0
Alex Axthelm <AAxthelm@che.in.gov>
2017-08-23 11:34:27 -0400

    Format `prune_envir` for lintr

--- R/envir.R
+++ R/envir.R
@@ -8,23 +8,33 @@ assign_to_envir <- Vectorize(
   c("target", "value")
   )
 
-prune_envir = function(targets, config){
-  downstream = lapply(targets, function(vertex)
-    subcomponent(config$graph, v = vertex, mode = "out")$name) %>%
-    unlist %>% unique
-  already_loaded = ls(envir = config$envir, all.names = TRUE) %>%
+prune_envir <- function(targets, config){
+  downstream <- lapply(
+    targets,
+    function(vertex){
+      subcomponent(config$graph, v = vertex, mode = "out")$name
+    }
+    ) %>%
+  unlist() %>%
+  unique()
+  already_loaded <- ls(envir = config$envir, all.names = TRUE) %>%
     intersect(y = config$plan$target)
-  load_these = 
-    nonfile_target_dependencies(targets = targets, config = config) %>%
+  load_these <- nonfile_target_dependencies(
+    targets = targets,
+    config = config
+    ) %>%
     setdiff(y = c(targets, already_loaded))
-  keep_these = 
-    nonfile_target_dependencies(targets = downstream, config = config)
-  discard_these = setdiff(x = config$plan$target, y = keep_these) %>%
-    Filter(f = is_not_file) %>% 
+  keep_these <- nonfile_target_dependencies(
+    targets = downstream,
+    config = config
+    )
+  discard_these <- setdiff(x = config$plan$target, y = keep_these) %>%
+    Filter(f = is_not_file) %>%
     intersect(y = already_loaded)
   rm(list = discard_these, envir = config$envir)
-  if(length(load_these)) 
+  if (length(load_these)){
     loadd(list = load_these, envir = config$envir)
+  }
   invisible()
 }
 
