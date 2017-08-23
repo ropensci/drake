c08e47398712bc6ab83d21e00cdbd2f662bd28a7
Alex Axthelm <AAxthelm@che.in.gov>
2017-08-23 11:20:52 -0400

    Format `fix_deprecated_plan_names` for lintr

--- R/deprecate.R
+++ R/deprecate.R
@@ -1,9 +1,10 @@
-fix_deprecated_plan_names = function(plan){
-  if(any(colnames(plan) %in% c("output", "code")))
+fix_deprecated_plan_names <- function(plan){
+  if (any(colnames(plan) %in% c("output", "code"))){
     warning("Drake is no longer using \"output\" or \"code\" ",
       "for column names in workflow plan data frames. Use \"target\" ",
       "and \"command\" instead.")
-  colnames(plan) = gsub("^output$", "target", colnames(plan)) %>%
+  }
+  colnames(plan) <- gsub("^output$", "target", colnames(plan)) %>%
     gsub(pattern = "^code$", replacement = "command")
   as.data.frame(plan, stringsAsFactors = FALSE)
 }
