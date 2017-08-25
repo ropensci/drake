7cd85b736ea45d45c7379ef0e38623a7b61d1a3b
C240390 <will.landau@lilly.com>
2017-08-25 08:08:22 -0400

    More forcing unlink

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,5 +3,8 @@ Sys.setenv("R_TESTS" = "")
 library(testthat)
 library(drake)
 
-unlink(file.path("testthat", "workspaces"), recursive = TRUE)
+unlink(
+  file.path("testthat", "workspaces"),
+  recursive = TRUE,
+  force = TRUE)
 test_check("drake")
