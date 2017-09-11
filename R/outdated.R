b72535efe0095596b96eb74d78a9c756427e8993
Alex Axthelm <AAxthelm@che.in.gov>
2017-09-11 11:42:59 -0400

    Format `outdated.R` for lintr (whitespace linelength)

--- R/outdated.R
+++ R/outdated.R
@@ -2,7 +2,7 @@
 #' @description Check which targets are out of date and need to be rebuilt.
 #' IMPORTANT: you must be in the root directory of your project.
 #' @export
-#' @seealso \code{\link{missed}}, \code{\link{plan}}, 
+#' @seealso \code{\link{missed}}, \code{\link{plan}},
 #' \code{\link{make}}, \code{\link{plot_graph}}
 #' @examples
 #' \dontrun{
@@ -19,11 +19,11 @@
 #' @param jobs same as for \code{\link{make}}
 #' @param packages same as for \code{\link{make}}
 #' @param prework same as for \code{\link{make}}
-#' @param config option internal runtime parameter list of 
+#' @param config option internal runtime parameter list of
 #' \code{\link{make}(...)},
 #' produced with \code{\link{config}()}.
 #' Computing this
-#' in advance could save time if you plan multiple calls to 
+#' in advance could save time if you plan multiple calls to
 #' \code{outdated()}.
 outdated <-  function(
   plan,
@@ -78,30 +78,31 @@ outdated <-  function(
 #' IMPORTANT: you must be in the root directory of your project.
 #' @export
 #' @seealso \code{\link{outdated}}
-#' @param plan workflow plan data frame, same as for function 
+#' @param plan workflow plan data frame, same as for function
 #' \code{\link{make}()}.
 #' @param targets names of targets to bulid, same as for function
 #' \code{\link{make}()}.
 #' @param envir environment to import from, same as for function
 #' \code{\link{make}()}.
 #' @param verbose logical, whether to output messages to the console.
-#' @param jobs The \code{outdated()} function is called internally, 
-#' and it needs to import objects and examine your 
+#' @param jobs The \code{outdated()} function is called internally,
+#' and it needs to import objects and examine your
 #' input files to see what has been updated. This could take some time,
 #' and parallel computing may be needed
-#' to speed up the process. The \code{jobs} argument is number of parallel jobs 
+#' to speed up the process. The \code{jobs} argument is number of parallel jobs
 #' to use for faster computation.
 #' @param parallelism Choice of parallel backend to speed up the computation.
-#' See \code{?parallelism_choices} for details. The Makefile option is not available
-#' here. Drake will try to pick the best option for your system by default.
+#' See \code{?parallelism_choices} for details. The Makefile option is not
+#' available here. Drake will try to pick the best option for your system by
+#' default.
 #' @param font_size numeric, font size of the node labels in the graph
 #' @param packages same as for \code{\link{make}}
 #' @param prework same as for \code{\link{make}}
-#' @param config option internal runtime parameter list of 
+#' @param config option internal runtime parameter list of
 #' \code{\link{make}(...)},
 #' produced with \code{\link{config}()}.
 #' Computing this
-#' in advance could save time if you plan multiple calls to 
+#' in advance could save time if you plan multiple calls to
 #' \code{missed()}.
 #' @examples
 #' \dontrun{
