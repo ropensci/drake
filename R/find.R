743b02c1756547dcf3f038e801d0f0cd856d31bc
Alex Axthelm <AAxthelm@che.in.gov>
2017-09-11 10:50:45 -0400

    Format `find_cache` for lintr

--- R/find.R
+++ R/find.R
@@ -13,14 +13,18 @@
 #' make(my_plan)
 #' find_cache()
 #' }
-find_cache = function(path = getwd()){
+find_cache <- function(path = getwd()){
   while (!(cache_dir %in% list.files(path = path, all.files = TRUE))){
-    path = dirname(path)
-    if (path == dirname(path)) return(NULL)
+    path <- dirname(path)
+    if (path == dirname(path)){
+      return(NULL)
+    }
   }
-  path = file.path(path, cache_dir)
-  if(!file.exists(path)) return(NULL)
-  path
+  path <- file.path(path, cache_dir)
+  if (!file.exists(path)){
+    return(NULL)
+  }
+  return(path)
 }
 
 #' @title Function \code{find_project}
