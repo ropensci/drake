b22832833468c5974e49fa1a50f0ce3abb378323
Alex Axthelm <AAxthelm@che.in.gov>
2017-09-11 11:16:58 -0400

    Format generate for lintr
    using dev version of lintr, which notices trailing pspace in roxygen
    comments

--- R/generate.R
+++ R/generate.R
@@ -1,35 +1,35 @@
 #' @title Function \code{evaluate}
-#' @description The commands in workflow plan data frames can have 
-#' wildcard symbols that can stand for datasets, parameters, function 
-#' arguments, etc. These wildcards can be evaluated over a set of 
-#' possible values using \code{evaluate}. 
+#' @description The commands in workflow plan data frames can have
+#' wildcard symbols that can stand for datasets, parameters, function
+#' arguments, etc. These wildcards can be evaluated over a set of
+#' possible values using \code{evaluate}.
 #' @details Specify a single wildcard with the \code{wildcard}
-#' and \code{values} arguments. In each command, the text in 
+#' and \code{values} arguments. In each command, the text in
 #' \code{wildcard} will be replaced by each value in \code{values}
 #' in turn. Specify multiple wildcards ith the \code{rules} argument,
-#' which overrules \code{wildcard} and \code{values} if 
+#' which overrules \code{wildcard} and \code{values} if
 #' not \code{NULL}. Here, \code{rules} should be a list with wildcards
 #' as names and vectors of possible values as list elements.
-#' @export 
+#' @export
 #' @return a workflow plan data frame with the wildcards evaluated
-#' @param plan workflow plan data frame, similar to one produced by 
+#' @param plan workflow plan data frame, similar to one produced by
 #' \code{link{plan}}
-#' @param rules Named list with wildcards as names and vectors of 
+#' @param rules Named list with wildcards as names and vectors of
 #' replacements
 #' as values. This is a way to evaluate multiple wildcards at once.
-#' When not \code{NULL}, \code{rules} overrules \code{wildcard} and 
+#' When not \code{NULL}, \code{rules} overrules \code{wildcard} and
 #' \code{values} if
-#' not \code{NULL}. 
+#' not \code{NULL}.
 #' @param wildcard character scalar denoting a wildcard placeholder
-#' @param values vector of values to replace the wildcard 
+#' @param values vector of values to replace the wildcard
 #' in the drake instructions. Will be treated as a character vector.
-#' Must be the same length as \code{plan$command} if \code{expand} is 
+#' Must be the same length as \code{plan$command} if \code{expand} is
 #' \code{TRUE}.
-#' @param expand If \code{TRUE}, create a new rows in the workflow plan 
+#' @param expand If \code{TRUE}, create a new rows in the workflow plan
 #' data frame
 #' if multiple values are assigned to a single wildcard.
 #' If \code{FALSE}, each occurence of the wildcard
-#' is replaced with the next entry in the \code{values} vector, 
+#' is replaced with the next entry in the \code{values} vector,
 #' and the values are recycled.
 #' @examples
 #' datasets <- plan(
@@ -107,12 +107,12 @@ evaluations <- function(
 #' @title Function \code{expand}
 #' @description Expands a workflow plan data frame by duplicating rows.
 #' This generates multiple replicates of targets with the same commands.
-#' @export 
+#' @export
 #' @return an expanded workflow plan data frame
 #' @param plan workflow plan data frame
 #' @param values values to expand over. These will be appended to
 #' the names of the new targets.
-#' @examples 
+#' @examples
 #' datasets <- plan(
 #'   small = simulate(5),
 #'   large = simulate(50))
@@ -131,14 +131,14 @@ expand <- function(plan, values = NULL){
 }
 
 #' @title Function \code{gather}
-#' @description Create a new workflow plan data frame with a single new 
+#' @description Create a new workflow plan data frame with a single new
 #' target. This new target is a list, vector, or other aggregate of
 #' a collection of existing targets in another workflow plan data frame.
-#' @export 
+#' @export
 #' @return workflow plan data frame for aggregating prespecified targets
 #' @param plan workflow plan data frame of prespecified targets
 #' @param target name of the new aggregated target
-#' @param gather function used to gather the targets. Should be 
+#' @param gather function used to gather the targets. Should be
 #' one of \code{\link{list}(...)}, \code{\link{c}(...)},
 #' \code{\link{rbind}(...)}, or similar.
 #' @examples
@@ -161,19 +161,19 @@ gather <- function(
 }
 
 #' @title Function \code{analyses}
-#' @description Generate a workflow plan data frame to 
+#' @description Generate a workflow plan data frame to
 #' analyze multiple datasets using multiple methods of analysis.
 #' @seealso \code{\link{summaries}},
 #'  \code{\link{make}}, \code{\link{plan}}
-#' @export 
+#' @export
 #' @return an evaluated workflow plan data frame of analysis instructions
-#' @param plan workflow plan data frame of analysis methods. 
+#' @param plan workflow plan data frame of analysis methods.
 #' The commands in the \code{command} column must
 #' have the \code{..dataset..} wildcard where the datasets go.
 #' For example, one command could be \code{lm(..dataset..)}. Then,
 #' the commands in the output will include \code{lm(your_dataset_1)},
-#' \code{lm(your_dataset_2)}, etc. 
-#' @param datasets workflow plan data frame with instructions 
+#' \code{lm(your_dataset_2)}, etc.
+#' @param datasets workflow plan data frame with instructions
 #' to make the datasets.
 #' @examples
 #' datasets <- plan(
@@ -195,18 +195,18 @@ analyses <- function(plan, datasets){
 #' @description Generate a worklfow plan data frame for summarizing
 #' multiple analyses of multiple datasets multiple ways.
 #' @seealso \code{\link{analyses}}, \code{\link{make}}, \code{\link{plan}}
-#' @export 
+#' @export
 #' @return an evaluated workflow plan data frame of instructions
 #' for computing summaries of analyses and datasets.
 #' analyses of multiple datasets in multiple ways.
 #' @param plan workflow plan data frame with commands for the summaries.
-#' Use the \code{..analysis..} and \code{..dataset..} wildcards 
-#' just like the \code{..dataset..} wildcard in \code{\link{analyses}()}. 
+#' Use the \code{..analysis..} and \code{..dataset..} wildcards
+#' just like the \code{..dataset..} wildcard in \code{\link{analyses}()}.
 #' @param analyses workflow plan data frame of analysis instructions
 #' @param datasets workflow plan data frame with instructions to make
 #' or import the datasets.
-#' @param gather Character vector, names of functions to gather the 
-#' summaries. If not \code{NULL}, the length must be the number of 
+#' @param gather Character vector, names of functions to gather the
+#' summaries. If not \code{NULL}, the length must be the number of
 #' rows in the \code{plan}. See the \code{\link{gather}()} function
 #' for more.
 #' @examples
@@ -220,7 +220,7 @@ analyses <- function(plan, datasets){
 #' summary_types <- plan(
 #'   summ = summary(..analysis..),
 #'   coef = coef(..analysis..))
-#' summaries(summary_types, analyses, datasets, gather = NULL) 
+#' summaries(summary_types, analyses, datasets, gather = NULL)
 #' summaries(summary_types, analyses, datasets)
 #' summaries(summary_types, analyses, datasets, gather = "list")
 #' summaries(summary_types, analyses, datasets, gather = c("list", "rbind"))
@@ -286,7 +286,8 @@ with_analyses_only <- function(plan){
 }
 
 unique_random_string <- function(exclude = NULL, n = 30){
-  while ( (out <- stri_rand_strings(1, n)) %in% exclude){
+  out <- stri_rand_strings(1, n)
+  while (out %in% exclude){
     next
   }
   return(out)
