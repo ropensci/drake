ba84599ec1adc9c4893f4a026af3e6a92e625b4f
wlandau-lilly <will.landau@lilly.com>
2017-09-22 19:23:01 -0400

    Try using the cache as is

--- inst/back-compatibility/v4.1.0/run.R
+++ inst/back-compatibility/v4.1.0/run.R
@@ -4,17 +4,5 @@ my_plan <- data.frame(
   target = "x",
   command = "readRDS('my_file.rds')"
 )
-con <- make(my_plan, return_config = TRUE, verbose = FALSE)
-clean_out <- c(
-  "config",
-  "progress",
-  "build_times"
-)
-for (ns in clean_out){
-  for (key in con$cache$list(namespace = ns)){
-    con$cache$del(key = key, namespace = ns)
-  }
-  path <- paste0(".drake", "keys", ns)
-  unlink(path, recursive = TRUE)
-}
+make(my_plan, verbose = FALSE)
 file.rename(from = ".drake", to = "cache")
