9c27b7d61746262dcc853be52ffb80caa15954c4
wlandau-lilly <will.landau@lilly.com>
2017-09-22 12:45:06 -0400

    Lint test-Makefile.R

--- tests/testthat/test-Makefile.R
+++ tests/testthat/test-Makefile.R
@@ -1,28 +1,29 @@
-# library(testthat); library(devtools); load_all()
 context("Makefile")
 
 test_with_dir("prepend arg works", {
   dclean()
-  config = dbug()
-  config$verbose = FALSE
-  config$prepend = "# add"
+  config <- dbug()
+  config$verbose <- FALSE
+  config$prepend <- "# add"
   run_Makefile(config, run = FALSE)
-  lines = readLines("Makefile")
+  lines <- readLines("Makefile")
   expect_true(grepl("# add", lines[1]))
   dclean()
 })
 
 test_with_dir("files inside directories can be timestamped", {
   dclean()
-  plan = plan(list = c("'t1/t2'" = 
-    'dir.create("t1"); saveRDS(1, file.path("t1", "t2"))'))
-  plan$target[1] = file = eply::quotes(file.path("t1", "t2"), 
-    single = TRUE)
-  config = build_config(plan = plan, targets = plan$target[1],
+  plan <- plan(
+    list = c(
+      `'t1/t2'` = "dir.create(\"t1\"); saveRDS(1, file.path(\"t1\", \"t2\"))"
+    )
+  )
+  plan$target[1] <- file <- eply::quotes(file.path("t1",
+    "t2"), single = TRUE)
+  config <- build_config(plan = plan, targets = plan$target[1],
     parallelism = "parLapply", verbose = FALSE, packages = character(0),
-    prework = character(0), prepend = character(0),
-    command = character(0), args = character(0), envir = new.env(),
-    jobs = 1)
+    prework = character(0), prepend = character(0), command = character(0),
+    args = character(0), envir = new.env(), jobs = 1)
   run_Makefile(config, run = FALSE)
   expect_silent(mk(config$plan$target[1]))
   expect_true(file.exists("t1"))
@@ -40,17 +41,19 @@ test_with_dir("files inside directories can be timestamped", {
 
 test_with_dir("basic Makefile stuff works", {
   dclean()
-  config = dbug()
-  make(config$plan, targets = "combined", 
-    envir = config$envir, verbose = FALSE)
-  config$verbose = FALSE
+  config <- dbug()
+  make(config$plan, targets = "combined", envir = config$envir,
+  verbose = FALSE)
+  config$verbose <- FALSE
   run_Makefile(config, run = FALSE, debug = TRUE)
-  using_global = identical(config$envir, globalenv())
-  if(using_global) expect_true(file.exists(globalenvpath))
+  using_global <- identical(config$envir, globalenv())
+  if (using_global) {
+    expect_true(file.exists(globalenvpath))
+  }
   expect_true(file.exists("Makefile"))
-  stamps = sort(list.files(file.path(time_stamp_dir), full.names = TRUE))
-  stamps2 = sort(time_stamp(c("combined", "myinput", "nextone", 
-    "yourinput")))
+  stamps <- sort(list.files(file.path(time_stamp_dir), full.names = TRUE))
+  stamps2 <- sort(time_stamp(c("combined", "myinput", "nextone",
+  "yourinput")))
   expect_equal(stamps, stamps2)
   expect_false(file.exists("intermediatefile.rds"))
   mk("'intermediatefile.rds'")
@@ -63,24 +66,23 @@ test_with_dir("basic Makefile stuff works", {
 
 test_with_dir("packages are loaded in prework", {
   dclean()
-  original = getOption("testdrake")
+  original <- getOption("testdrake")
   options(testdrake = "unset")
   expect_equal(getOption("testdrake"), "unset")
-  config = dbug()
-  if(R.utils::isPackageLoaded("abind"))
-    detach("package:abind")
-  if(R.utils::isPackageLoaded("MASS"))
-    detach("package:MASS")
+  config <- dbug()
+  if (R.utils::isPackageLoaded("abind"))
+  detach("package:abind")
+  if (R.utils::isPackageLoaded("MASS"))
+  detach("package:MASS")
   expect_error(abind(1))
   expect_error(deparse(body(lda)))
-  
+
   # Load packages with the 'packages' argument
-  config$packages = c("abind", "MASS")
-  config$prework = "options(testdrake = 'set')"
-  config$plan = plan(x = getOption("testdrake"),
-    y = c(abind("option"), deparse(body(lda)), x), 
-    strings_in_dots = "literals")
-  config$targets = config$plan$target
+  config$packages <- c("abind", "MASS")
+  config$prework <- "options(testdrake = 'set')"
+  config$plan <- plan(x = getOption("testdrake"), y = c(abind("option"),
+  deparse(body(lda)), x), strings_in_dots = "literals")
+  config$targets <- config$plan$target
   expect_false(any(c("x", "y") %in% config$cache$list()))
   testrun(config)
   expect_true(all(c("x", "y") %in% config$cache$list()))
@@ -88,28 +90,28 @@ test_with_dir("packages are loaded in prework", {
   expect_true(length(readd(y, search = FALSE)) > 0)
   options(testdrake = original)
   clean(search = FALSE)
-  
+
   # load packages the usual way
   options(testdrake = "unset")
   expect_equal(getOption("testdrake"), "unset")
-  if(R.utils::isPackageLoaded("abind"))
-    detach("package:abind")
-  if(R.utils::isPackageLoaded("MASS"))
-    detach("package:MASS")
+  if (R.utils::isPackageLoaded("abind"))
+  detach("package:abind")
+  if (R.utils::isPackageLoaded("MASS"))
+  detach("package:MASS")
   expect_error(abind(1))
   expect_error(deparse(body(lda)))
   library(abind)
   library(MASS)
-  config$packages = NULL
+  config$packages <- NULL
   expect_false(any(c("x", "y") %in% config$cache$list()))
-  opt = test_opt()
-  suppressWarnings( # drake may be loaded but not installed.
-    make(plan = config$plan, targets = config$targets,
-      envir = config$envir, verbose = FALSE,
-      parallelism = opt$parallelism, jobs = opt$jobs,
-      prework = config$prework,
-      prepend = config$prepend, command = config$command)
-  )
+  opt <- test_opt()
+
+  # drake may be loaded with devtools::load_all() but not
+  # installed.
+  suppressWarnings(make(plan = config$plan, targets = config$targets,
+  envir = config$envir, verbose = FALSE, parallelism = opt$parallelism,
+  jobs = opt$jobs, prework = config$prework, prepend = config$prepend,
+  command = config$command))
   expect_true(all(c("x", "y") %in% config$cache$list()))
   expect_equal(readd(x, search = FALSE), "set")
   expect_true(length(readd(y, search = FALSE)) > 0)
