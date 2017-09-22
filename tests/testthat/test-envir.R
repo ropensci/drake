207cd457164db7e65dfa875a433a3fa4c778b74d
wlandau-lilly <will.landau@lilly.com>
2017-09-22 12:25:42 -0400

    Lint test-envir.R and fix .lintr

--- tests/testthat/test-envir.R
+++ tests/testthat/test-envir.R
@@ -1,38 +1,55 @@
-# library(testthat); library(devtools); load_all()
 context("envir")
 
 test_with_dir("prune_envir in full build", {
   dclean()
-  
-  # workflow with lots of nested deps
-  # This will fail if prune_envir() doesn't work.
-  datasets = plan(x = 1, y = 2, z = 3)
-  methods = plan(a = ..dataset.., b = ..dataset.., c = ..dataset..)
-  analyses = analyses(methods, datasets)
-  heuristics = plan(s = c(..dataset.., ..analysis..), 
+
+  # workflow with lots of nested deps This will fail if
+  # prune_envir() doesn't work.
+  datasets <- plan(x = 1, y = 2, z = 3)
+  methods <- plan(
+    a = ..dataset..,
+    b = ..dataset..,
+    c = ..dataset..
+  )
+  analyses <- analyses(methods, datasets)
+  heuristics <- plan(
+    s = c(..dataset.., ..analysis..),
     t = ..analysis..)
-  summaries = summaries(heuristics, datasets = datasets, 
-    analyses = analyses, gather = c("rbind", "rbind"))
-  output = plan(final1 = mean(s)+mean(t), 
-    final2 = mean(s)-mean(t), 
+  summaries <- summaries(
+    heuristics,
+    datasets = datasets,
+    analyses = analyses,
+    gather = c("rbind", "rbind")
+  )
+  output <- plan(
+    final1 = mean(s) + mean(t),
+    final2 = mean(s) - mean(t),
     waitforme = c(a_x, c_y, s_b_x, t_a_z),
-    waitformetoo = c(waitforme, y))
-  plan = rbind(datasets, analyses, summaries, output)
-  
+    waitformetoo = c(waitforme, y)
+  )
+  plan <- rbind(datasets, analyses, summaries, output)
+
   # set up a workspace to test prune_envir()
-  config = build_config(plan, targets = plan$target, 
-    envir = new.env(parent = globalenv()), 
-    parallelism = "mclapply", jobs = 1, prepend = character(0),
-    verbose = TRUE, packages = character(0), 
-    prework = character(0), 
-    command = "make", args = character(0))
-  
+  config <- build_config(
+    plan,
+    targets = plan$target,
+    envir = new.env(parent = globalenv()),
+    parallelism = "mclapply",
+    jobs = 1,
+    prepend = character(0),
+    verbose = TRUE,
+    packages = character(0),
+    prework = character(0),
+    command = "make",
+    args = character(0)
+  )
+
   # actually run
   testrun(config)
   expect_true(all(plan$target %in% cached()))
-  
-  # Check that the right things are loaded 
-  # and the right things are discarded
+
+  # Check that the right things are loaded and the right things
+  # are discarded
   remove(list = ls(config$envir), envir = config$envir)
   expect_equal(ls(config$envir), character(0))
   prune_envir(datasets$target, config)
@@ -40,10 +57,12 @@ test_with_dir("prune_envir in full build", {
   prune_envir(analyses$target, config)
   expect_equal(ls(config$envir), c("x", "y", "z"))
   prune_envir("waitforme", config)
-  
+
   # keep y around for waitformetoo
-  expect_equal(ls(config$envir),
-    c("a_x", "c_y", "s_b_x", "t_a_z", "y"))
-  
+  expect_equal(
+    ls(config$envir),
+    c("a_x", "c_y", "s_b_x", "t_a_z", "y")
+  )
+
   dclean()
 })
