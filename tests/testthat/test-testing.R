0a4b57a620c664411ead1692b73f483f4cc8697a
wlandau-lilly <will.landau@lilly.com>
2017-09-25 09:41:35 -0400

    Test that test_with_dir() contains code in testing envir
    See test-testing.R

--- tests/testthat/test-testing.R
+++ tests/testthat/test-testing.R
@@ -10,3 +10,14 @@ test_with_dir("testing configs are there", {
   options(drake_test_opt = old_opt)
   expect_equal(old_opt, getOption("drake_test_opt"))
 })
+
+test_with_dir("test_with_dir() evaluates inside the testing envir", {
+  some_outside_object <- 4
+  test_with_dir("nested test", {
+    some_nested_object <- 1
+    expect_true("some_nested_object" %in% ls())
+    expect_false("some_outside_object" %in% ls())
+  })
+  expect_false("some_nested_object" %in% ls())
+  expect_true("some_outside_object" %in% ls())
+})
