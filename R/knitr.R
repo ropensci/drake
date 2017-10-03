d1864c42db47924c9784d7809209269ccf226d51
wlandau-lilly <will.landau@lilly.com>
2017-10-03 12:01:05 -0400

    Start on #9

---
+++ R/knitr.R
@@ -0,0 +1,79 @@
+knitr_dependencies <- function(target){
+  file <- unquote(target)
+  fragments <- get_tangled_frags(file)
+  find_knitr_targets(fragments)
+}
+
+# From https://github.com/duncantl/CodeDepends/blob/master/R/sweave.R#L15
+get_tangled_frags <- function(doc, txt = readLines(doc)) {
+  in.con <- textConnection(txt)
+  out.con <- textConnection("bob", "w", local = TRUE)
+  on.exit({
+    close(in.con)
+    close(out.con)
+  })
+  knitr::knit(in.con, output = out.con, tangle = TRUE, quiet = TRUE)
+  code <- textConnectionValue(out.con)
+  parse(text = code)
+}
+
+find_knitr_targets <- function(expr, targets = character(0)){
+  if (is.function(expr)){
+    return(find_knitr_targets(body(expr), targets = targets))
+  } else if (is.call(expr) & length(expr) > 1){
+    targets <- c(targets, analyze_loadd(expr), analyze_readd(expr))
+    targets <- as.list(expr) %>%
+      Filter(f = found_loadd_readd) %>%
+      lapply_find_knitr_targets(targets = targets)
+  } else if (is.recursive(expr)){
+    targets <- lapply_find_knitr_targets(
+      list = as.list(expr), targets = targets)
+  }
+  targets
+}
+
+analyze_loadd <- function(expr){
+  if (deparse(expr[[1]]) != "loadd"){
+    return()
+  }
+  args <- as.list(expr)[-1]
+  targets <- unnamed_in_list(args)
+  list <- get_specific_arg(args = args, name = "list")
+  c(targets, list)
+}
+
+analyze_readd <- function(expr){
+  if (deparse(expr[[1]]) != "readd"){
+    return()
+  }
+  args <- as.list(expr)[-1]
+  targets <- unnamed_in_list(args)
+  target <- get_specific_arg(args = args, name = "target")
+  c(targets, target)
+}
+
+found_loadd_readd <- function(x){
+  grepl("readd|loadd", wide_deparse(x))
+}
+
+get_specific_arg <- function(args, name){
+  tryCatch(
+    eval(args[[name]]),
+    error = function(e){
+      character(0)
+    }
+  )
+}
+
+lapply_find_knitr_targets <- function(list, targets){
+  v <- lapply(list, find_knitr_targets, targets = targets)
+  targets <- unique(c(targets, unlist(v)))
+}
+
+unnamed_in_list <- function(x){
+  if (!length(names(x))){
+    as.character(x)
+  } else {
+    as.character(x[!nchar(names(x))])
+  }
+}
