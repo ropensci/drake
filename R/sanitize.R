df27918059a7df793aab1de553fab104ed5bf978
wlandau-lilly <will.landau@lilly.com>
2017-10-27 21:28:01 -0400

    Add sanitize.R

---
+++ R/sanitize.R
@@ -0,0 +1,35 @@
+sanitize_plan <- function(plan){
+  for (field in c("code", "command", "output", "target")){
+    if (!is.null(plan[[field]])){
+      plan[[field]] <- str_trim(plan[[field]], side = "both")
+    }
+  }
+  as.data.frame(plan, stringsAsFactors = FALSE) %>%
+    fix_deprecated_plan_names()
+}
+
+sanitize_nodes <- function(nodes, choices){
+  if (!any(nodes %in% choices)){
+    stop(
+      "All import/target names are invalid ",
+      "in argument 'targets', 'to', or 'from' ",
+      "for make(), plot_graph(), or similar.",
+      call. = FALSE
+    )
+  }
+  diffs <- setdiff(nodes, choices)
+  if (length(diffs)){
+    warning(
+      "Ignoring imports/targets that were requeted but not found:\n",
+      multiline_message(diffs),
+      call. = FALSE
+    )
+  }
+  intersect(nodes, choices)
+}
+
+sanitize_targets <- function(plan, targets){
+  plan <- sanitize_plan(plan)
+  targets <- str_trim(targets, side = "both")
+  sanitize_nodes(nodes = targets, choices = plan$target)
+}
