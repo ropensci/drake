bd91a70ed9669070f28750194c07fc2b4f550a00
wlandau-lilly <will.landau@lilly.com>
2017-10-29 14:05:48 -0400

    Add comments to shell and sge files

--- inst/shell.sh
+++ inst/shell.sh
@@ -1,12 +1,16 @@
 # Optionally use this file to send targets
 # to different jobs on a cluster.
+# Usage from within R:
 #
-# library(drake)
-# ...
 # shell_file() # Copy this file to your working directory.
 # make(my_plan, parallelism = "Makefile",
 #   prepend = "SHELL=./shell.sh", ...)
 #
+# Below, you may have to change 'module load R'
+# to 'module load R-3.4.2' or similar.
+# Just be sure to name the correct environment module.
+# See 'module avail' or 'module avail R'.
+# 
 #!/bin/bash
 shift
 echo "module load R; $*" | qsub -sync y -cwd -j y
