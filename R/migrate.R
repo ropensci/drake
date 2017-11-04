36255ccb36f2026d5ea8d0a917bd0a9a7c9ced3f
wlandau-lilly <will.landau@lilly.com>
2017-11-04 08:22:23 -0400

    Sketch migrate() supporting functions

--- R/migrate.R
+++ R/migrate.R
@@ -1,35 +1,3 @@
-assert_compatible_cache <- function(cache){
-  if (is.null(cache)){
-    return()
-  }
-  err <- try(
-    old <- session(cache = cache)$otherPkgs$drake$Version, silent = TRUE) # nolint
-  if (inherits(err, "try-error")){
-    return()
-  }
-  comparison <- compareVersion(old, "4.4.0")
-  if (comparison > 0){
-    return(invisible())
-  }
-  current <- packageVersion("drake")
-  path <- cache$driver$path
-  newpath <- backup_cache_path(path = path, old = old)
-  stop(
-    "The project at '", path, "' was previously built by drake ", old, ". ",
-    "You are running drake ", current, ", which is not back-compatible. ",
-    "To format your cache for the newer drake, try migrate('", path, "'). ",
-    "migrate() restructures the cache in a way that ",
-    "preserves the statuses of your targets (up to date vs outdated). ",
-    "But in case of errors, migrate() first backs up '", path, "' to '",
-    newpath, "'. Alternatively, you can just run your project from scratch ",
-    "as is with make(..., force = TRUE)."
-  )
-}
-
-backup_cache_path <- function(path, old){
-  newpath <- paste0(path, "_backup_drake_", old)
-}
-
 #' @title Function migrate
 #' @export
 #' @seealso \code{\link{rescue_cache}}, \code{\link{make}}
@@ -41,19 +9,51 @@ backup_cache_path <- function(path, old){
 #' with projects built with a later version of drake. migrate() converts
 #' an old cache to a format compatible with the version of drake
 #' installed on your system.
-migrate <- function(path){
-  cache <- this_cache(path = path)
+migrate <- function(path = drake::default_cache_path()){
+  do_migrate <- should_migrate(path = path, throw_error = FALSE)
+  if (!do_migrate){
+    cat("No need to migrate the cache. Returning.\n")
+  }
+  
+}
+
+assert_compatible_cache <- function(cache){
+  tmp <- should_migrate(cache = cache, throw_error = TRUE)
+  invisible()
+}
+
+backup_cache_path <- function(path, old){
+  newpath <- paste0(path, "_backup_drake_", old)
+}
+
+should_migrate <- function(path = NULL, cache = drake::this_cache(path = path),
+  throw_error = TRUE){
   if (is.null(cache)){
     return()
   }
   err <- try(
     old <- session(cache = cache)$otherPkgs$drake$Version, silent = TRUE) # nolint
   if (inherits(err, "try-error")){
-    return()
+    return(FALSE)
   }
   comparison <- compareVersion(old, "4.4.0")
   if (comparison > 0){
-    return(invisible())
+    return(FALSE)
   }
-  
+  if (throw_error){
+    current <- packageVersion("drake")
+    path <- cache$driver$path
+    newpath <- backup_cache_path(path = path, old = old)
+    stop(
+      "The project at '", path, "' was previously built by drake ", old, ". ",
+      "You are running drake ", current, ", which is not back-compatible. ",
+      "To format your cache for the newer drake, try migrate('", path, "'). ",
+      "migrate() restructures the cache in a way that ",
+      "preserves the statuses of your targets (up to date vs outdated). ",
+      "But in case of errors, migrate() first backs up '", path, "' to '",
+      newpath, "'. Alternatively, you can just run your project from scratch ",
+      "as is with make(..., force = TRUE)."
+    )
+  }
+  TRUE
 }
