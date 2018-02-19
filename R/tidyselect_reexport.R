de5f4672786c882652325e415f6f83c0cbe2b87c
wlandau-lilly <will.landau@lilly.com>
2018-02-19 14:44:39 -0500

    Fix reexports

---
+++ R/tidyselect_reexport.R
@@ -0,0 +1,24 @@
+# From `dplyr`:
+# Flag to disable hotpatching from old tidyselect versions
+peek_vars <- tidyselect::peek_vars
+#' @importFrom tidyselect contains
+#' @export
+tidyselect::contains
+#' @importFrom tidyselect ends_with
+#' @export
+tidyselect::ends_with
+#' @importFrom tidyselect everything
+#' @export
+tidyselect::everything
+#' @importFrom tidyselect matches
+#' @export
+tidyselect::matches
+#' @importFrom tidyselect num_range
+#' @export
+tidyselect::num_range
+#' @importFrom tidyselect one_of
+#' @export
+tidyselect::one_of
+#' @importFrom tidyselect starts_with
+#' @export
+tidyselect::starts_with
