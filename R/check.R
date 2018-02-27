6c2e14a5a406784c8369d740fe1bfc876edb1e18
wlandau-lilly <will.landau@lilly.com>
2018-02-27 12:23:15 -0500

    Fix linting

--- R/check.R
+++ R/check.R
@@ -116,8 +116,8 @@ check_jobs <- function(jobs){
   stopifnot(length(jobs) > 0)
   stopifnot(is.numeric(jobs) || is.integer(jobs))
   stopifnot(all(jobs > 0))
-  if(!is.null(names(jobs))){
-    if(!identical(sort(names(jobs)), sort(c("imports", "targets")))){
+  if (!is.null(names(jobs))){
+    if (!identical(sort(names(jobs)), sort(c("imports", "targets")))){
       stop(
         "In the `jobs` argument, you must either give an unnamed numeric ",
         "or a named numeric with names 'imports' and 'targets'.",
