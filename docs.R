c6576e8a08e93d0703af4ce28489856529d6bda4
wlandau <will.landau@gmail.com>
2018-03-11 19:52:27 -0400

    Tweak pkgdown site generation
    - Wrap `images.R` into `docs.R`.
    - Borrow `gh-pages.sh` from `storr` to deploy `docs`.
    - Use a top-level `images/` folder manually-created images such as logo.svg (copied into `docs/images/` in `docs.R`). That way, all of `docs/` is auto-generated.
    
    cc @krlmlr

--- docs.R
+++ docs.R
@@ -55,9 +55,6 @@ pkgdown <- function(){
   tmp <- pkgdown::build_site(pkg = dir, path = site_dir, preview = FALSE)
 
   x <- readLines(index_file)
-  icon <- file.path(dir, "docs/images", "icon.ico")
-  tmp <- file.copy(from = icon, to = site_dir, overwrite = TRUE)
-
   from <- "<p><img src=.*docs/images/graph.png.*></p>"
   to <- "<iframe
     src = 'https://ropensci.github.io/drake/images/reg2.html'
@@ -87,6 +84,140 @@ pkgdown <- function(){
   )
 }
 
+# Generate the HTML widgets in the docs/images/ folder.
+# These interactive graphs are embedded in the vignettes.
+# Requires pandoc.
+images <- function(){
+  library(here)
+  html_out <- function(...) here::here("docs", "images", ...)
+  for (dir in c(here::here("docs"), here::here("docs", "images"))){
+    if (!file.exists(dir)){
+      dir.create(dir)
+    }
+  }
+  for (img in list.files(here::here("images"))){
+    file.copy(
+      here::here("images", img),
+      here::here("docs", "images", img),
+      overwrite = TRUE
+    )
+  }
+  file.copy(
+    here::here("docs", "images", "icon.ico"),
+    here::here("docs", "icon.ico"),
+    overwrite = TRUE
+  )
+  devtools::load_all() # load current drake
+  clean(destroy = TRUE)
+  config <- load_basic_example(overwrite = TRUE)
+  vis_drake_graph(
+    config, file = html_out("outdated.html"), selfcontained = TRUE,
+                  width = "100%", height = "500px")
+  config <- make(my_plan)
+  vis_drake_graph(config, file = html_out("built.html"), selfcontained = TRUE,
+                  width = "100%", height = "500px")
+  reg2 <- function(d){
+    d$x3 <- d$x ^ 3
+    lm(y ~ x3, data = d)
+  }
+  vis_drake_graph(config, file = html_out("reg2.html"), selfcontained = TRUE,
+                  width = "100%", height = "500px")
+  vis_drake_graph(
+    config, file = html_out("reg2-small-legend.html"), selfcontained = TRUE,
+                  width = "100%", height = "500px", full_legend = FALSE)
+  vis_drake_graph(
+    config, file = html_out("reg2-no-legend.html"), selfcontained = TRUE,
+                  width = "100%", height = "500px", ncol_legend = 0)
+  vis_drake_graph(
+    config, file = html_out("targetsonly.html"), selfcontained = TRUE,
+    targets_only = TRUE,
+    width = "100%", height = "500px",
+    from = c("large", "small")
+  )
+  vis_drake_graph(
+    config, file = html_out("fromout.html"), selfcontained = TRUE,
+    width = "100%", height = "500px",
+    from = c("regression2_small", "regression2_large")
+  )
+  vis_drake_graph(
+    config, file = html_out("fromin.html"), selfcontained = TRUE,
+    width = "100%", height = "500px",
+    from = "small", mode = "in"
+  )
+  vis_drake_graph(
+    config, file = html_out("fromall.html"), selfcontained = TRUE,
+    width = "100%", height = "500px",
+    from = "small", mode = "all", order = 1
+  )
+  vis_drake_graph(
+    config, file = html_out("subset.html"), selfcontained = TRUE,
+    width = "100%", height = "500px",
+    subset = c("regression2_small", "\"report.md\"")
+  )
+  clean(destroy = TRUE)
+  unlink("report.Rmd")
+
+  # For the "packages" example.
+  library(magrittr)
+  reportfile <- file.path("examples", "packages", "report.Rmd") %>%
+    system.file(package = "drake", mustWork = TRUE)
+  file.copy(reportfile, getwd())
+  runfile <- file.path("examples", "packages", "interactive-tutorial.R") %>%
+    system.file(package = "drake", mustWork = TRUE)
+  source(runfile)
+  vis_drake_graph(
+    config, file = html_out("packages.html"), selfcontained = TRUE,
+    width = "100%", height = "500px"
+  )
+
+  # For the best practices vignette
+  get_data <- function(){
+    "Get the data."
+  }
+  analyze_data <- function(){
+    "Analyze the data."
+  }
+  summarize_results <- function(){
+    "Summarize the results."
+  }
+  files <- c("data.csv", "get_data.R", "analyze_data.R", "summarize_data.R")
+  for (file in files){
+    file.create(file)
+  }
+  my_plan <- drake_plan(
+    my_data = get_data(file_in("data.csv")),
+    my_analysis = analyze_data(my_data),
+    my_summaries = summarize_results(my_data, my_analysis),
+    strings_in_dots = "literals"
+  )
+  config <- drake_config(my_plan)
+  vis_drake_graph(
+    main = "Good workflow plan",
+    config, file = html_out("good-commands.html"), selfcontained = TRUE,
+    width = "100%", height = "500px"
+  )
+  my_plan <- drake_plan(
+    my_data = source(file_in("get_data.R")),
+    my_analysis = source(file_in("analyze_data.R")),
+    my_summaries = source(file_in("summarize_data.R")),
+    strings_in_dots = "literals"
+  )
+  config <- drake_config(my_plan)
+  vis_drake_graph(
+    main = "Bad workflow plan",
+    config, file = html_out("bad-commands.html"), selfcontained = TRUE,
+    width = "100%", height = "500px"
+  )
+
+  # Clean up.
+  for (file in files){
+    file.remove(file)
+  }
+  clean(destroy = TRUE)
+  unlink(c("figure", "report.Rmd"), recursive = TRUE)
+  unlink(html_out("*_files"), recursive = TRUE)
+}
+
 faq()
 pkgdown()
-source("images.R", local = TRUE)
+images()
