409cb2f9beabc2990a2cd02c3829d62628091c02
wlandau <will.landau@gmail.com>
2018-03-16 18:16:25 -0400

    Update pkgdown site
    Using pkgdown CRAN RC

--- docs.R
+++ docs.R
@@ -46,13 +46,12 @@ faq <- function(){
 
 pkgdown <- function(){
   dir <- rprojroot::find_root(criterion = "DESCRIPTION", path = getwd())
-  dest <- file.path(dir, "docs")
-  if (!file.exists(dest)){
-    dir.create(dest)
+  site_dir <- file.path(dir, "docs")
+  if (!file.exists(site_dir)){
+    dir.create(site_dir)
   }
-  site_dir <- tempdir()
   index_file <- file.path(site_dir, "index.html")
-  tmp <- pkgdown::build_site(pkg = dir, path = site_dir, preview = FALSE)
+  tmp <- pkgdown::build_site(pkg = dir, preview = FALSE)
 
   x <- readLines(index_file)
   from <- '<p><a href="https://ropensci.github.io/drake/images/reg2.html"><img src="./docs/images/graph.png"></a></p>' # nolint
@@ -72,13 +71,11 @@ pkgdown <- function(){
   x <- gsub(pattern = from, replacement = to, x = x)
 
   tmp <- writeLines(text = x, con = index_file)
-  from <- list.files(site_dir, full.names = TRUE)
-  tmp <- file.copy(from = from, to = dest, overwrite = TRUE, recursive = TRUE)
   unlink(
     c(
-      file.path(dest, "*.rds"),
-      file.path(dest, "file*"),
-      file.path(dest, "preview-")
+      file.path(site_dir, "*.rds"),
+      file.path(site_dir, "file*"),
+      file.path(site_dir, "preview-")
     ),
     recursive = TRUE
   )
--- docs/pkgdown.js
+++ docs/pkgdown.js
@@ -1,5 +1,14 @@
 $(function() {
-  $("#sidebar").stick_in_parent({offset_top: 40});
+
+  $("#sidebar")
+    .stick_in_parent({offset_top: 40})
+    .on('sticky_kit:bottom', function(e) {
+      $(this).parent().css('position', 'static');
+    })
+    .on('sticky_kit:unbottom', function(e) {
+      $(this).parent().css('position', 'relative');
+    });
+
   $('body').scrollspy({
     target: '#sidebar',
     offset: 60
@@ -36,6 +45,11 @@ function is_prefix(needle, haystack) {
   if (needle.length > haystack.lengh)
     return(false);
 
+  // Special case for length-0 haystack, since for loop won't run
+  if (haystack.length === 0) {
+    return(needle.length === 0);
+  }
+
   for (var i = 0; i < haystack.length; i++) {
     if (needle[i] != haystack[i])
       return(false);
@@ -43,3 +57,43 @@ function is_prefix(needle, haystack) {
 
   return(true);
 }
+
+/* Clipboard --------------------------*/
+
+function changeTooltipMessage(element, msg) {
+  var tooltipOriginalTitle=element.getAttribute('data-original-title');
+  element.setAttribute('data-original-title', msg);
+  $(element).tooltip('show');
+  element.setAttribute('data-original-title', tooltipOriginalTitle);
+}
+
+if(Clipboard.isSupported()) {
+  $(document).ready(function() {
+    var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' type = 'submit' title='Copy to clipboard' aria-hidden='true' data-toggle='tooltip' data-placement='left auto' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy' aria-hidden='true'></i></button>";
+
+    $(".examples").addClass("hasCopyButton");
+
+    // Insert copy buttons:
+    $(copyButton).prependTo(".hasCopyButton");
+
+    // Initialize tooltips:
+    $('.btn-copy-ex').tooltip({container: 'body'});
+
+    // Initialize clipboard:
+    var clipboardBtnCopies = new Clipboard('[data-clipboard-copy]', {
+      text: function(trigger) {
+        return trigger.parentNode.textContent;
+      }
+    });
+
+    clipboardBtnCopies.on('success', function(e) {
+      changeTooltipMessage(e.trigger, 'Copied!');
+      e.clearSelection();
+    });
+
+    clipboardBtnCopies.on('error', function() {
+      changeTooltipMessage(e.trigger,'Press Ctrl+C or Command+C to copy');
+    });
+  });
+}
+
