6bab31f8edb9cbfa034e3e198bd89ee5d4f67986
wlandau <will.landau@gmail.com>
2018-03-28 22:42:25 -0400

    Safeguard a test

--- tests/testthat/test-other-features.R
+++ tests/testthat/test-other-features.R
@@ -248,7 +248,7 @@ test_with_dir("make(session = callr::r_vanilla)", {
   make_with_config(con)
   expect_equal(sort(justbuilt(con)), sort(con$plan$target))
   ls2 <- ls(envir = con$envir)
-  expect_equal(ls1, ls2)
+  expect_equal(sort(ls1), sort(ls2))
   rm_these <- setdiff(ls(envir = con$envir), ls1)
   if(length(rm_these)){
     rm(list = rm_these, envir = con$envir)
