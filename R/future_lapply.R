db5bd2ed0080d942365ed84aa5c584d39bb541f2
wlandau <will.landau@gmail.com>
2018-05-03 00:15:28 -0400

    Improve sketch of future_lapply backend
    Use callr::r_bg for the master process there.

--- R/future_lapply.R
+++ R/future_lapply.R
@@ -2,27 +2,21 @@ run_future_lapply <- function(config){
   prepare_distributed(config = config)
   config$workers <- as.character(seq_len(config$jobs))
   mc_init_worker_cache(config)
+  callr::r_bg(func = mc_master, args = list(config = config))
   tmp <- future.apply::future_lapply(
-    X = c("0", config$workers),
-    FUN = fl_process,
+    X = config$workers,
+    FUN = fl_worker,
     cache_path = config$cache$driver$path,
     future.globals = FALSE
   )
   finish_distributed(config = config)
 }
 
-fl_process <- function(id, cache_path){
-  config <- recover_drake_config(cache_path = cache_path)
-  try_message({
-    if (id == "0"){
-      mc_master(config)
-    } else {
-      fl_worker(worker = id, config = config)
-    }
-  })
-}
-
 fl_worker <- function(worker, config){
-  do_prework(config = config, verbose_packages = FALSE)
-  mc_worker(worker = worker, config = config)
+  try_message({
+    config <- recover_drake_config(cache_path = cache_path)
+    config$schedule <- targets_graph(config)
+    do_prework(config = config, verbose_packages = FALSE)
+    mc_worker(worker = worker, config = config)
+  })
 }
