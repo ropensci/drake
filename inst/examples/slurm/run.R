5867d2f6e30f0c0578c8329bc6a0e99b8915a01e
wlandau <will.landau@gmail.com>
2018-05-13 00:26:45 -0400

    Touch up docs

--- inst/examples/slurm/run.R
+++ inst/examples/slurm/run.R
@@ -6,14 +6,7 @@ library(future.batchtools)
 library(drake)
 
 # Use future::plan(multicore) instead for a dry run.
-future::plan(
-  batchtools_slurm,
-  template = "batchtools.slurm.tmpl",
-  workers = 4
-)
+future::plan(batchtools_slurm, template = "batchtools.slurm.tmpl")
 
 load_mtcars_example()
-make(
-  my_plan,
-  parallelism = "future_lapply"
-)
+make(my_plan, parallelism = "future", jobs = 4)
