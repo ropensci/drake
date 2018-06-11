177a7aa43c421b2c046403eeec61a0895befb14b
wlandau <will.landau@gmail.com>
2018-06-10 21:33:59 -0400

    Try to redeploy site

---
+++ docs.R
@@ -0,0 +1,4 @@
+rmarkdown::render("README.Rmd")
+unlink("docs", recursive = TRUE)
+pkgdown::build_site()
+fs::dir_copy("images", "docs")
--- pkgdown.R
+++
@@ -1,2 +0,0 @@
-pkgdown::build_site()
-fs::dir_copy("images", "docs")
