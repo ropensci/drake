cc7f073e98cb1a20389c9997d62cdd5b358242ad
wlandau-lilly <will.landau@lilly.com>
2018-07-14 22:35:08 -0400

    Enhance the file_out() test (test-intermediate-file.R)

--- tests/testthat/test-intermediate-file.R
+++ tests/testthat/test-intermediate-file.R
@@ -6,49 +6,58 @@ test_with_dir("responses to intermediate file", {
   envir <- dbug_envir(envir)
   dbug_files()
   plan <- dbug_plan()
-  plan$command[1] <- wide_deparse(quote({
+  command1 <- wide_deparse(quote({
     saveRDS(combined, file_out("intermediatefile.rds"))
     saveRDS(combined + 1, file_out("out2.rds"))
   }))
-  plan$command[6] <- wide_deparse(quote({
-    readRDS(file_in("intermediatefile.rds")) +
-    readRDS(file_in("out2.rds"))
+  command2 <- wide_deparse(quote({
+    saveRDS(combined, "intermediatefile.rds")
+    saveRDS(combined + 1, "out2.rds")
+    file_out("intermediatefile.rds", "out2.rds")
   }))
-  config <- drake_config(plan = plan, targets = plan$target,
-    envir = envir, parallelism = scenario$parallelism,
-    jobs = scenario$jobs, verbose = FALSE,
-    session_info = FALSE,
-    log_progress = TRUE,
-    caching = scenario$caching
-  )
-  testrun(config)
-  expect_equal(justbuilt(config), sort(config$plan$target))
-  expect_equal(outdated(config), character(0))
-
-  # check missing and then replace file exactly as before
-  final0 <- readd(final, search = FALSE)
-  val <- readRDS("intermediatefile.rds")
-  val2 <- readRDS("out2.rds")
-  expect_equal(val + 1, val2)
-  unlink("intermediatefile.rds", force = TRUE)
-  saveRDS(val, "intermediatefile.rds")
-  testrun(config)
-  nobuild(config)
-  expect_equal(final0, readd(final, search = FALSE))
-
-  # actually change a file
-  for (file in c("intermediatefile.rds", "out2.rds")){
-    saveRDS(sum(val) + 100, file)
+  for (command in c(command1, command2)){
+    plan$command[1] <- command
+    plan$command[6] <- wide_deparse(quote({
+      readRDS(file_in("intermediatefile.rds")) +
+      readRDS(file_in("out2.rds"))
+    }))
+    config <- drake_config(plan = plan, targets = plan$target,
+      envir = envir, parallelism = scenario$parallelism,
+      jobs = scenario$jobs, verbose = FALSE,
+      session_info = FALSE,
+      log_progress = TRUE,
+      caching = scenario$caching
+    )
     testrun(config)
-    expect_equal(justbuilt(config), "drake_target_1")
-    expect_equal(final0, readd(final, search = FALSE))
-  }
+    expect_equal(justbuilt(config), sort(config$plan$target))
+    expect_equal(outdated(config), character(0))
 
-  # break a file
-  for (file in c("intermediatefile.rds", "out2.rds")){
-    unlink(file, force = TRUE)
+    # check missing and then replace file exactly as before
+    final0 <- readd(final, search = FALSE)
+    val <- readRDS("intermediatefile.rds")
+    val2 <- readRDS("out2.rds")
+    expect_equal(val + 1, val2)
+    unlink("intermediatefile.rds", force = TRUE)
+    saveRDS(val, "intermediatefile.rds")
     testrun(config)
-    expect_equal(justbuilt(config), "drake_target_1")
+    nobuild(config)
     expect_equal(final0, readd(final, search = FALSE))
+
+    # actually change a file
+    for (file in c("intermediatefile.rds", "out2.rds")){
+      saveRDS(sum(val) + 100, file)
+      testrun(config)
+      expect_equal(justbuilt(config), "drake_target_1")
+      expect_equal(final0, readd(final, search = FALSE))
+    }
+
+    # break a file
+    for (file in c("intermediatefile.rds", "out2.rds")){
+      unlink(file, force = TRUE)
+      testrun(config)
+      expect_equal(justbuilt(config), "drake_target_1")
+      expect_equal(final0, readd(final, search = FALSE))
+    }
+    clean(destroy = TRUE)
   }
 })
