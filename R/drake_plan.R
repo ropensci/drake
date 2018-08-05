a87959af738933bd6dd1248fb8779f49a8cd8801
wlandau <will.landau@gmail.com>
2018-08-04 22:38:00 -0400

    Build drake_plan() call

--- R/drake_plan.R
+++ R/drake_plan.R
@@ -568,3 +568,25 @@ target <- function(
     }) %>%
     tibble::as_tibble()
 }
+
+drake_plan_call <- function(plan){
+  target_calls <- purrr::pmap(plan, drake_target_call)
+  as.call(c(quote(drake_plan), target_calls))
+}
+
+drake_target_call <- function(...){
+  args <- list(...)[drake_plan_columns()] %>%
+    select_valid()
+  target <- parse(text = args$target)[[1]]
+  args$target <- NULL
+  if (!is.null(args$command) && is.character(args$command)){
+    args$command <- parse(text = args$command)[[1]]
+  }
+  if (!is.null(args$trigger) && is.character(args$trigger)){
+    args$trigger <- parse(text = args$trigger)[[1]]
+  }
+  if (!identical(names(args), "command")){
+    args$command <- as.call(c(quote(target), args))
+  }
+  call("=", target, args$command)
+}
