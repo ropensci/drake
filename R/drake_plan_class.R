27c7b39a2ba3e9726774cb317c5e878161620167
wlandau <will.landau@gmail.com>
2018-08-05 07:36:57 -0400

    Update more docs

--- R/drake_plan_class.R
+++ R/drake_plan_class.R
@@ -20,27 +20,19 @@ as_drake_plan_ <- function(x, ...){
 }
 
 #' @export
+#' @rdname as_drake_plan
 `[.drake_plan` <- function(...){
   as_drake_plan_(NextMethod())
 }
 
 #' @export
+#' @rdname as_drake_plan
 as_drake_plan.data.frame <- as_drake_plan_
 
 #' @export
+#' @rdname as_drake_plan
 as_drake_plan.list <- as_drake_plan_
 
 #' @export
+#' @rdname as_drake_plan
 as_drake_plan.tbl_df <- as_drake_plan_
-
-#' @export
-as.data.frame.drake_plan <- as_drake_plan_
-
-#' @export
-as_data_frame.drake_plan <- as_drake_plan_
-
-#' @export
-as_tibble.drake_plan <- as_drake_plan_
-
-#' @export
-as.tibble.drake_plan <- as_drake_plan_
