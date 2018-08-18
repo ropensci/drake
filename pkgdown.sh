62727ed5fe02f254452c07b369db3d77369e298e
wlandau <will.landau@gmail.com>
2018-08-18 12:05:54 -0400

    Fix pkgdown site issues

--- pkgdown.sh
+++ pkgdown.sh
@@ -2,7 +2,12 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+echo "Environment variables:"
+echo "  PATH: ${PATH}"
+echo "  TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+echo "  TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+echo "  TRAVIS_PULL_REQUEST_BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}"
+[ "${TRAVIS_BRANCH}" != "master" || "${TRAVIS_PULL_REQUEST}" == "true" ] && exit 0
 
 git config --global user.email "will.landau@gmail.com"
 git config --global user.name "wlandau"
