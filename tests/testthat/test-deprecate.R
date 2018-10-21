6e62f3bc5a9c5d342ab3c9617e0b0a80e9c314f4
wlandau <will.landau@gmail.com>
2018-10-21 07:21:03 -0400

    Tweak a test

--- tests/testthat/test-deprecate.R
+++ tests/testthat/test-deprecate.R
@@ -284,7 +284,6 @@ test_with_dir("old trigger interface", {
 
 test_with_dir("mtcars example", {
   skip_on_cran()
-  skip_if_not_installed("downloader")
   expect_warning(
     load_mtcars_example(report_file = "other_name.Rmd"),
     regexp = "report_file"
