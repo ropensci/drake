285b8178e6f62d0322ffa621fb9bec13e1bc80d0
wlandau <will.landau@gmail.com>
2018-10-21 04:51:09 -0400

    Condense load_mtcars_example()

--- R/mtcars_example.R
+++ R/mtcars_example.R
@@ -58,95 +58,18 @@ load_mtcars_example <- function(
   overwrite = FALSE,
   force = FALSE
 ){
-  eval(parse(text = "base::require(drake, quietly = TRUE)"))
-  eval(parse(text = "base::require(knitr, quietly = TRUE)"))
-  mtcars <- get("mtcars")
-
-  # Pick a random subset of n rows from a dataset
-  evalq(
-    random_rows <- function(data, n){
-      data[sample.int(n = nrow(data), size = n, replace = TRUE), ]
-    },
-    envir = envir
+  dir <- tempfile()
+  drake_example(example = "mtcars", to = dir)
+  source(file.path(dir, "mtcars", "R", "packages.R"), local = envir)
+  source(file.path(dir, "mtcars", "R", "functions.R"), local = envir)
+  envir$my_plan <- source(
+    file.path(dir, "mtcars", "R", "plan.R"),
+    local = TRUE
+  )$value
+  file.copy(
+    from = file.path(dir, "mtcars", "report.Rmd"),
+    to = "report.Rmd",
+    overwrite = overwrite
   )
-
-  # Bootstrapped datasets from mtcars.
-  evalq(
-    simulate <- function(n){
-      # Pick a random set of cars to bootstrap from the mtcars data.
-      data <- random_rows(data = mtcars, n = n)
-
-      # x is the car's weight, and y is the fuel efficiency.
-      data.frame(
-        x = data$wt,
-        y = data$mpg
-      )
-    },
-    envir = envir
-  )
-
-  # Is there a linear relationship between weight and fuel efficiency?
-  evalq(
-    reg1 <- function(d) {
-      lm(y ~ +x, data = d)
-    },
-    envir = envir
-  )
-
-  # Is there a QUADRATIC relationship between weight and fuel efficiency?
-  evalq(
-    reg2 <- function(d) {
-      d$x2 <- d$x ^ 2
-      lm(y ~ x2, data = d)
-    },
-    envir = envir
-  )
-
-  # construct workflow plan
-
-  # remove 'undefinded globals' errors in R CMD check
-  dataset__ <- analysis__ <- large <- small <-
-    simulate <- knit <- my_knit <- report_dependencies <-
-    reg1 <- reg2 <- coef_regression2_small <- NULL
-
-  datasets <- drake_plan(small = simulate(48), large = simulate(64))
-
-  methods <- drake_plan(
-    regression1 = reg1(dataset__),
-    regression2 = reg2(dataset__)
-  )
-
-  # Same as evaluate_plan(methods, wildcard = 'dataset__',
-  #   values = datasets$output).
-  analyses <- plan_analyses(methods, datasets = datasets)
-
-  summary_types <- drake_plan(
-    summ = suppressWarnings(summary(analysis__$residuals)), # Summarize the RESIDUALS of the model fit. # nolint
-    coef = suppressWarnings(summary(analysis__))$coefficients # Coefficinents with p-values # nolint
-  )
-
-  # plan_summaries() also uses evaluate_plan(): once with expand = TRUE,
-  # once with expand = FALSE
-  # skip 'gather' (drake_plan my_plan is more readable)
-  results <- plan_summaries(summary_types, analyses, datasets, gather = NULL)
-
-  report <- tibble(
-    target = "report",
-    command = 'knit(knitr_in("report.Rmd"), file_out("report.md"), quiet = TRUE)' # nolint  
-  )
-
-  # Row order doesn't matter in the drake_plan my_plan.
-  envir$my_plan <- rbind(report, datasets, analyses, results)
-
-  # Write the R Markdown source for a dynamic knitr report
-  report <- system.file(
-    file.path("rmarkdown", "mtcars.Rmd"),
-    package = "drake",
-    mustWork = TRUE
-  )
-  if (file.exists(report_file) & overwrite){
-    warning("Overwriting file 'report.Rmd'.")
-  }
-  file.copy(from = report, to = report_file, overwrite = overwrite)
   invisible()
 }
