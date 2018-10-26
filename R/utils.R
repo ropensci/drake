7123f14747a1d0c53963128e627227d775228bec
wlandau-lilly <will.landau@lilly.com>
2018-10-26 16:13:35 -0400

    Rearrange functions in utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,12 @@
+# From lintr
+`%||%` <- function(x, y) {
+  if (is.null(x) || length(x) <= 0) {
+    y
+  } else {
+    x
+  }
+}
+
 assert_pkg <- function(pkg, version = NULL, install = "install.packages") {
   if (!requireNamespace(pkg, quietly = TRUE)){
     stop(
@@ -21,63 +30,8 @@ assert_pkg <- function(pkg, version = NULL, install = "install.packages") {
   }
 }
 
-safe_grepl <- function(pattern, x, ...){
-  tryCatch(grepl(pattern, x, ...), error = error_false)
-}
-
-safe_is_na <- function(x){
-  tryCatch(is.na(x), error = error_false, warning = error_false)
-}
-
-is_file <- function(x){
-  safe_grepl(pattern = quotes_regex, x = x)
-}
-
-standardize_filename <- function(text){
-  text[is_file(text)] <-  gsub("^'|'$", "\"", text[is_file(text)])
-  text
-}
-
-is_not_file <- function(x){
-  !is_file(x)
-}
-
-rehash_file_size_cutoff <- 1e5
-
 braces <- function(x) {
   paste("{\n", x, "\n}")
-}
-
-# From lintr
-`%||%` <- function(x, y) {
-  if (is.null(x) || length(x) <= 0) {
-    y
-  } else {
-    x
-  }
-}
-
-drake_select <- function(
-  cache, ..., namespaces = cache$default_namespace, list = character(0)
-){
-  tidyselect::vars_select(
-    .vars = list_multiple_namespaces(cache = cache, namespaces = namespaces),
-    ...,
-    .strict = FALSE
-  ) %>%
-    unname %>%
-    c(list)
-}
-
-merge_lists <- function(x, y){
-  names <- base::union(names(x), names(y))
-  lapply(
-    X = names,
-    function(name){
-      base::union(x[[name]], y[[name]])
-    }
-  ) %>%
-    setNames(nm = names)
 }
 
 clean_dependency_list <- function(x){
@@ -92,10 +46,62 @@ clean_dependency_list <- function(x){
     sort()
 }
 
+drake_select <- function(
+  cache, ..., namespaces = cache$default_namespace, list = character(0)
+){
+  tidyselect::vars_select(
+    .vars = list_multiple_namespaces(cache = cache, namespaces = namespaces),
+    ...,
+    .strict = FALSE
+  ) %>%
+    unname %>%
+    c(list)
+}
+
+factor_to_character <- function(x){
+  if (is.factor(x)){
+    x <- as.character(x)
+  }
+  x
+}
+
+is_file <- function(x){
+  safe_grepl(pattern = quotes_regex, x = x)
+}
+
+is_image_filename <- function(x){
+  tolower(fs::path_ext(x)) %in% c("jpg", "jpeg", "pdf", "png")
+}
+
+is_not_file <- function(x){
+  !is_file(x)
+}
+
+merge_lists <- function(x, y){
+  names <- base::union(names(x), names(y))
+  lapply(
+    X = names,
+    function(name){
+      base::union(x[[name]], y[[name]])
+    }
+  ) %>%
+    setNames(nm = names)
+}
+
 padded_scale <- function(x){
   r <- range(x)
   pad <- 0.2 * (r[2] - r[1])
   c(r[1] - pad, r[2] + pad)
+}
+
+rehash_file_size_cutoff <- 1e5
+
+safe_grepl <- function(pattern, x, ...){
+  tryCatch(grepl(pattern, x, ...), error = error_false)
+}
+
+safe_is_na <- function(x){
+  tryCatch(is.na(x), error = error_false, warning = error_false)
 }
 
 select_nonempty <- function(x){
@@ -120,13 +126,7 @@ select_valid <- function(x){
   x[index]
 }
 
-is_image_filename <- function(x){
-  tolower(fs::path_ext(x)) %in% c("jpg", "jpeg", "pdf", "png")
-}
-
-factor_to_character <- function(x){
-  if (is.factor(x)){
-    x <- as.character(x)
-  }
-  x
+standardize_filename <- function(text){
+  text[is_file(text)] <-  gsub("^'|'$", "\"", text[is_file(text)])
+  text
 }
