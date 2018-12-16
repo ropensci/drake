ae4f4b841b878be953192c76baa8724efcd69a9d
wlandau <will.landau@gmail.com>
2018-12-15 21:41:05 -0500

    Fix #562

--- tests/testthat/test-deprecate.R
+++ tests/testthat/test-deprecate.R
@@ -259,36 +259,6 @@ test_with_dir("force with a non-back-compatible cache", {
   expect_silent(tmp <- recover_cache())
 })
 
-test_with_dir("v6.2.1 project is still up to date", {
-  skip_on_cran() # CRAN gets whitelist tests only (check time limits).
-  skip_if_not_installed("tibble")
-  write_v6.2.1_project() # nolint
-  report_md_hash <- readRDS(
-    file.path(".drake", "data", "983396f9689f587b.rds")
-  )
-  expect_equal(nchar(report_md_hash), 64L)
-  plan <- read_drake_plan()
-  random_rows <- function(data, n) {
-    data[sample.int(n = nrow(data), size = n, replace = TRUE), ]
-  }
-  simulate <- function(n) {
-    data <- random_rows(data = mtcars, n = n)
-    data.frame(
-      x = data$wt,
-      y = data$mpg
-    )
-  }
-  reg1 <- function(d) {
-    lm(y ~ + x, data = d)
-  }
-  reg2 <- function(d) {
-    d$x2 <- d$x ^ 2
-    lm(y ~ x2, data = d)
-  }
-  config <- make(plan)
-  expect_equal(justbuilt(config), character(0))
-})
-
 test_with_dir("deprecate the `force` argument", {
   expect_warning(tmp <- get_cache(force = TRUE), regexp = "deprecated")
   expect_warning(tmp <- this_cache(force = TRUE), regexp = "deprecated")
