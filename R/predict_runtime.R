a9f8b511ecc80944e4376358a3467da156374f2a
wlandau <will.landau@gmail.com>
2018-12-20 22:20:18 -0500

    standardize keys of known times

--- R/predict_runtime.R
+++ R/predict_runtime.R
@@ -178,6 +178,7 @@ predict_load_balancing <- function(
   default_time = 0,
   warn = TRUE
 ) {
+  names(known_times) <- standardize_key(names(known_times))
   assumptions <- timing_assumptions(
     config = config,
     targets = targets,
