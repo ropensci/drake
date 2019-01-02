30770b99bc45d5452b19afacb0b960d34d04c8e5
wlandau <will.landau@gmail.com>
2019-01-02 07:06:08 -0500

    Fixes

--- R/test.R
+++ R/test.R
@@ -4,6 +4,27 @@ drake_context <- function(x) {
   testthat::context(ctx)
 }
 
+testconfig <- function(config) {
+  drake_config(
+    plan = config$plan,
+    targets = config$targets,
+    envir = config$envir,
+    verbose = config$verbose,
+    parallelism = config$parallelism,
+    jobs = config$jobs,
+    packages = config$packages,
+    prework = config$prework,
+    prepend = config$prepend,
+    command = config$command,
+    cache = config$cache,
+    lazy_load = config$lazy_load,
+    session_info = config$session_info,
+    fetch_cache = config$fetch_cache,
+    caching = config$caching,
+    lock_envir = !any(grepl("staged", config$parallelism))
+  )
+}
+
 testrun <- function(config) {
   set_test_backend()
   invisible(
