88e156aff3515c65296c3702cf1dbcc9b811c5a3
wlandau <will.landau@gmail.com>
2019-01-09 21:24:18 -0500

    Finish sketch. Now to test...

--- R/vis-graphinfo.R
+++ R/vis-graphinfo.R
@@ -160,9 +160,9 @@ drake_graph_info <- function(
     vertices <- vertices[!(!imported & is_encoded_path(vertices))]
     config$graph <- subset_graph(config$graph, vertices)
   }
-  config$graph <- get_neighborhood(
+  config$graph <- nbhd_graph(
     graph = config$graph,
-    from = from,
+    vertices = from,
     mode = match.arg(mode),
     order = order
   )
