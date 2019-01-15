457988a3cb2e2b6ef156caa3177aea6cf6f9caf2
wlandau <will.landau@gmail.com>
2019-01-14 22:57:08 -0500

    Shorten names

--- tests/testthat/test-transform.R
+++ tests/testthat/test-transform.R
@@ -8,12 +8,12 @@ test_with_dir("transformations of plans", {
       reg_fun(data),
       transform = cross(reg_fun = c(reg1, reg2), data = c(small, large))
     ),
-    summary = target(
+    summ = target(
       sum_fun(data, reg),
-      transform = cross(sum_fun = c(coefficients, residuals), reg)
+      transform = cross(sum_fun = c(coef, residuals), reg)
     ),
     winners = target(
-      min(summary),
+      min(summ),
       transform = summarize(data, sum_fun)
     )
   )
@@ -24,33 +24,32 @@ test_with_dir("transformations of plans", {
     reg_reg2_small = reg2(small),
     reg_reg1_large = reg1(large),
     reg_reg2_large = reg2(large),
-    summary_coefficients_reg_reg1_large = coefficients(large, reg_reg1_large),
-    summary_residuals_reg_reg1_large = residuals(large, reg_reg1_large),
-    summary_coefficients_reg_reg1_small = coefficients(small, reg_reg1_small),
-    summary_residuals_reg_reg1_small = residuals(small, reg_reg1_small),
-    summary_coefficients_reg_reg2_large = coefficients(large, reg_reg2_large),
-    summary_residuals_reg_reg2_large = residuals(large, reg_reg2_large),
-    summary_coefficients_reg_reg2_small = coefficients(small, reg_reg2_small),
-    summary_residuals_reg_reg2_small = residuals(small, reg_reg2_small),
-    winners_large_coefficients = min(
-      summary_coefficients_reg_reg1_large =
-        summary_coefficients_reg_reg1_large,
-      summary_coefficients_reg_reg2_large =
-        summary_coefficients_reg_reg2_large
+    summ_coef_reg_reg1_large = coef(large, reg_reg1_large),
+    summ_residuals_reg_reg1_large = residuals(large, reg_reg1_large),
+    summ_coef_reg_reg1_small = coef(small, reg_reg1_small),
+    summ_residuals_reg_reg1_small = residuals(small, reg_reg1_small),
+    summ_coef_reg_reg2_large = coef(large, reg_reg2_large),
+    summ_residuals_reg_reg2_large = residuals(large, reg_reg2_large),
+    summ_coef_reg_reg2_small = coef(small, reg_reg2_small),
+    summ_residuals_reg_reg2_small = residuals(small, reg_reg2_small),
+    winners_large_coef = min(
+      summ_coef_reg_reg1_large = summ_coef_reg_reg1_large,
+      summ_coef_reg_reg2_large = summ_coef_reg_reg2_large
     ),
-    winners_small_coefficients = min(
-      summary_coefficients_reg_reg1_small =
-        summary_coefficients_reg_reg1_small,
-      summary_coefficients_reg_reg2_small =summary_coefficients_reg_reg2_small
+    winners_small_coef = min(
+      summ_coef_reg_reg1_small = summ_coef_reg_reg1_small,
+      summ_coef_reg_reg2_small = summ_coef_reg_reg2_small
     ),
     winners_large_residuals = min(
-      summary_residuals_reg_reg1_large = summary_residuals_reg_reg1_large,
-      summary_residuals_reg_reg2_large = summary_residuals_reg_reg2_large
+      summ_residuals_reg_reg1_large = summ_residuals_reg_reg1_large,
+      summ_residuals_reg_reg2_large = summ_residuals_reg_reg2_large
     ),
     winners_small_residuals = min(
-      summary_residuals_reg_reg1_small = summary_residuals_reg_reg1_small,
-      summary_residuals_reg_reg2_small = summary_residuals_reg_reg2_small
+      summ_residuals_reg_reg1_small = summ_residuals_reg_reg1_small,
+      summ_residuals_reg_reg2_small = summ_residuals_reg_reg2_small
     )
   )
-  expect_equivalent(out[order(out$target), ], exp[order(exp$target), ])
+  out$command <- lapply(out$command, standardize_command)
+  exp$command <- lapply(exp$command, standardize_command)
+  expect_equal(out[order(out$target), ], exp[order(exp$target), ])
 })
