36ac2ca881331b539ad7ba3136885d7a9faad9df
wlandau <will.landau@gmail.com>
2019-03-09 14:58:36 -0500

    Skip a test earlier

--- tests/testthat/test-zzz-callr.R
+++ tests/testthat/test-zzz-callr.R
@@ -29,6 +29,7 @@ test_with_dir("basic functions with default _drake.R file", {
   expect_true(
     is.data.frame(r_drake_build("small", r_args = list(show = FALSE)))
   )
+  skip_if_not_installed("visNetwork")
   info <- r_drake_graph_info(r_args = list(show = FALSE))
   expect_true(all(c("nodes", "edges") %in% names(info)))
   out <- r_predict_workers(r_args = list(show = FALSE))
@@ -44,7 +45,6 @@ test_with_dir("basic functions with default _drake.R file", {
   # need automated visual tests
   skip_if_not_installed("ggraph")
   skip_if_not_installed("networkD3")
-  skip_if_not_installed("visNetwork")
   expect_silent({
     r_vis_drake_graph(r_args = list(show = FALSE))
     r_vis_drake_graph(
