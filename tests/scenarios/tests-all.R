d552b94708c6996dba254df53ca0e9b6f2138d6c
wlandau <will.landau@gmail.com>
2019-04-04 10:04:37 -0500

    Set opts for testing

--- tests/scenarios/tests-all.R
+++ tests/scenarios/tests-all.R
@@ -1,5 +1,6 @@
 Sys.setenv(NOT_CRAN = "true")
 Sys.setenv(drake_skip_callr = "true")
+options(drake_make_menu = FALSE, drake_clean_menu = FALSE)
 library(testthat)
 devtools::load_all()
 args <- commandArgs(TRUE)
