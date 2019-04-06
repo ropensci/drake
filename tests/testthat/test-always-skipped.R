6eb1cde2ba980727c6abee9d5117f15774a488ac
wlandau <will.landau@gmail.com>
2019-04-06 01:54:38 -0400

    Update the drake_debug() unit test

--- tests/testthat/test-always-skipped.R
+++ tests/testthat/test-always-skipped.R
@@ -35,19 +35,20 @@ test_with_dir("can keep going in parallel", {
 test_with_dir("drake_debug()", {
   skip_on_cran()
   load_mtcars_example()
-  my_plan$command[2] <- "simulate(48); stop(1234)"
+  my_plan$command[[2]] <- quote({
+    simulate(48)
+    stop(1234)
+  })
   config <- drake_config(my_plan, lock_envir = TRUE)
   expect_error(make(my_plan), regexp = "1234")
   out <- drake_debug(large, config = config)
   out <- drake_debug(
-    "large", config = config, verbose = 0L, character_only = TRUE)
-  expect_true(is.data.frame(out))
-  my_plan$command <- lapply(
-    X = as.list(my_plan$command),
-    FUN = function(x) {
-      parse(text = x)[[1]]
-    }
+    "large",
+    config = config,
+    verbose = 0L,
+    character_only = TRUE
   )
+  expect_true(is.data.frame(out))
   for (i in 1:2) {
     clean(destroy = TRUE)
     load_mtcars_example()
