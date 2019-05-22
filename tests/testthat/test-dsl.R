45889f06d6ddf3adbb72ad888d7a65784871579b
wlandau-lilly <will.landau@lilly.com>
2019-05-22 14:24:25 -0400

    Test split()

--- tests/testthat/test-dsl.R
+++ tests/testthat/test-dsl.R
@@ -2413,5 +2413,91 @@ test_with_dir("basic splitting", {
       transform = combine(slice_analysis)
     )
   )
+  exp <- drake_plan(
+    large_data = get_data(),
+    slice_analysis_1 = drake_slice(
+      data = large_data, slices = 4, index = 1) %>% analyze(),
+    slice_analysis_2 = drake_slice(
+      data = large_data, slices = 4, index = 2) %>% analyze(),
+    slice_analysis_3 = drake_slice(
+      data = large_data, slices = 4, index = 3) %>% analyze(),
+    slice_analysis_4 = drake_slice(
+      data = large_data, slices = 4, index = 4) %>% analyze(),
+    results = dplyr::bind_rows(
+      slice_analysis_1, slice_analysis_2, slice_analysis_3,
+      slice_analysis_4
+    )
+  )
+  equivalent_plans(out, exp)
+})
 
+test_with_dir("splitting with all args", {
+  out <- drake_plan(
+    large_data = get_data(),
+    slice_analysis = target(
+      large_data %>%
+        analyze(),
+      transform = split(large_data, margin = 2, drop = TRUE, slices = 4)
+    ),
+    results = target(
+      dplyr::bind_rows(slice_analysis),
+      transform = combine(slice_analysis)
+    )
+  )
+  exp <- drake_plan(
+    large_data = get_data(),
+    slice_analysis_1 = drake_slice(
+      data = large_data, slices = 4, index = 1, margin = 2,
+      drop = TRUE
+    ) %>% analyze(),
+    slice_analysis_2 = drake_slice(
+      data = large_data, slices = 4, index = 2, margin = 2,
+      drop = TRUE
+    ) %>% analyze(),
+    slice_analysis_3 = drake_slice(
+      data = large_data, slices = 4, index = 3, margin = 2,
+      drop = TRUE
+    ) %>% analyze(),
+    slice_analysis_4 = drake_slice(
+      data = large_data, slices = 4, index = 4, margin = 2,
+      drop = TRUE
+    ) %>% analyze(),
+    results = dplyr::bind_rows(
+      slice_analysis_1, slice_analysis_2, slice_analysis_3,
+      slice_analysis_4
+    )
+  )
+  equivalent_plans(out, exp)
+})
+
+test_with_dir("splitting with tidy eval", {
+  s <- 4
+  out <- drake_plan(
+    large_data = get_data(),
+    slice_analysis = target(
+      large_data %>%
+        analyze(),
+      transform = split(large_data, slices = !!s)
+    ),
+    results = target(
+      dplyr::bind_rows(slice_analysis),
+      transform = combine(slice_analysis)
+    )
+  )
+  exp <- drake_plan(
+    large_data = get_data(),
+    slice_analysis_1 = drake_slice(
+      data = large_data, slices = 4, index = 1) %>% analyze(),
+    slice_analysis_2 = drake_slice(
+      data = large_data, slices = 4, index = 2) %>% analyze(),
+    slice_analysis_3 = drake_slice(
+      data = large_data, slices = 4, index = 3) %>% analyze(),
+    slice_analysis_4 = drake_slice(
+      data = large_data, slices = 4, index = 4) %>% analyze(),
+    results = dplyr::bind_rows(
+      slice_analysis_1, slice_analysis_2, slice_analysis_3,
+      slice_analysis_4
+    )
+  )
+  equivalent_plans(out, exp)
 })
