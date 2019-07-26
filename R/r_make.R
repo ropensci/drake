b5c9b5af800c1d71d109058d57ad02fb72b84ba1
wlandau-lilly <will.landau@lilly.com>
2019-07-26 10:19:22 -0400

    Organize callr fns

--- R/r_make.R
+++ R/r_make.R
@@ -1,39 +1,3 @@
-default_drake_source <- "_drake.R"
-
-r_drake <- function(source, d_fn, d_args, r_fn, r_args) {
-  assert_pkg("callr")
-  r_args$func <- function(source, d_fn, d_args) {
-    d_args$config <- source(source)$value
-    do.call(d_fn, d_args)
-  }
-  source <- source %||% getOption("drake_source") %||% default_drake_source
-  r_assert_source(source)
-  r_args$args <- list(source = source, d_fn = d_fn, d_args = d_args)
-  r_fn <- r_fn %||% callr::r
-  if ("show" %in% names(formals(r_fn))) {
-    r_args$show <- r_args$show %||% TRUE
-  }
-  do.call(r_fn, r_args)
-}
-
-r_assert_source <- function(source) {
-  if (file.exists(source)) {
-    return()
-  }
-  stop(
-    "File ", shQuote(source), " does not exist.\n",
-    "Functions r_make() and friends need an R script file\n",
-    "to configure your drake project.\n",
-    "The configuration R script can be\n",
-    "  1. The file you supply to the ", shQuote(source), " argument, or\n",
-    "  2. The file you supply via options(drake_source = \"file.R\"), or\n",
-    "  3. A file called ", shQuote("_drake.R"), " (default).\n",
-    "Read more: \n",
-    "https://ropenscilabs.github.io/drake-manual/projects.html#safer-interactivity", # nolint
-    call. = FALSE
-  )
-}
-
 #' @title Reproducible R session management for drake functions
 #' @description `drake` searches your environment
 #'   to detect dependencies, so functions like [make()], [outdated()], etc.
@@ -236,4 +200,40 @@ r_predict_workers <- function(
   ..., source = NULL, r_fn = NULL, r_args = list()
 ) {
   r_drake(source, drake::predict_workers, list(...), r_fn, r_args)
+}
+
+r_drake <- function(source, d_fn, d_args, r_fn, r_args) {
+  assert_pkg("callr")
+  r_args$func <- function(source, d_fn, d_args) {
+    d_args$config <- source(source)$value
+    do.call(d_fn, d_args)
+  }
+  source <- source %||% getOption("drake_source") %||% default_drake_source
+  r_assert_source(source)
+  r_args$args <- list(source = source, d_fn = d_fn, d_args = d_args)
+  r_fn <- r_fn %||% callr::r
+  if ("show" %in% names(formals(r_fn))) {
+    r_args$show <- r_args$show %||% TRUE
+  }
+  do.call(r_fn, r_args)
+}
+
+default_drake_source <- "_drake.R"
+
+r_assert_source <- function(source) {
+  if (file.exists(source)) {
+    return()
+  }
+  stop(
+    "File ", shQuote(source), " does not exist.\n",
+    "Functions r_make() and friends need an R script file\n",
+    "to configure your drake project.\n",
+    "The configuration R script can be\n",
+    "  1. The file you supply to the ", shQuote(source), " argument, or\n",
+    "  2. The file you supply via options(drake_source = \"file.R\"), or\n",
+    "  3. A file called ", shQuote("_drake.R"), " (default).\n",
+    "Read more: \n",
+    "https://ropenscilabs.github.io/drake-manual/projects.html#safer-interactivity", # nolint
+    call. = FALSE
+  )
 }
