9b6aa8ef5dd8fdbe4a7a2b14d314a52bb12b41da
wlandau <will.landau@gmail.com>
2019-07-28 09:31:08 -0400

    Reorganize helper functions

--- tests/testthat/helper-scenarios.R
+++ tests/testthat/helper-scenarios.R
@@ -1,90 +1,3 @@
-default_testing_scenario <- "local_loop_1"
-test_option_name <- "drake_test_scenario"
-
-testing_scenarios <- function() {
-  file <- file.path("testing", "scenarios.csv")
-  path <- system.file(file, package = "drake", mustWork = TRUE)
-  x <- read.csv(path, stringsAsFactors = FALSE)
-  rn <- paste(
-    x$envir,
-    x$parallelism,
-    x$jobs,
-    x$backend,
-    x$caching,
-    sep = "_"
-  )
-  rn <- gsub(pattern = "_*$", replacement = "", x = rn)
-  rownames(x) <- rn
-  x$backend <- backend_code(x$backend)
-  x$envir <- envir_code(x$envir)
-  x$caching[!nzchar(x$caching)] <- "worker"
-  apply_skip_os(x)
-}
-
-backend_code <- function(x) {
-  ifelse(
-    nzchar(x),
-    paste0("future::plan(", x, ")"),
-    x
-  )
-}
-
-envir_code <- function(x) {
-  ifelse(
-    x == "local",
-    "new.env(parent = globalenv())",
-    "globalenv()"
-  )
-}
-
-# For the table of possible testing scenarios x.
-apply_skip_os <- function(x) {
-  x$skip_os <- ""
-  skip_on_windows <- grepl("mclapply|clustermq", x$parallelism)
-  x$skip_os[skip_on_windows] <- "windows"
-  x
-}
-
-testing_scenario_names <- function() {
-  rownames(testing_scenarios())
-}
-
-get_testing_scenario <- function() {
-  scenario <- get_testing_scenario_name()
-  testing_scenarios()[scenario, ]
-}
-
-get_testing_scenario_name <- function() {
-  scenario <- getOption(test_option_name)
-  if (!length(scenario)) {
-    scenario <- match.arg(
-      arg = default_testing_scenario,
-      choices = testing_scenario_names()
-    )
-  }
-  scenario
-}
-
-set_testing_scenario <- function(scenario = NULL) {
-  scenario <- match.arg(scenario, choices = testing_scenario_names())
-  new <- list()
-  new[[test_option_name]] <- scenario
-  options(new)
-}
-
-set_test_backend <- function() {
-  eval(parse(text = get_testing_scenario()$backend))
-}
-
-should_skip <- function(scenario_name, os = this_os()) {
-  scenarios <- testing_scenarios()
-  scenario_name <- match.arg(
-    arg = scenario_name,
-    choices = testing_scenario_names()
-  )
-  os %in% scenarios[scenario_name, ]$skip_os
-}
-
 test_scenarios <- function(
   scenario_names = testing_scenario_names(),
   unit_test_dir = unit_test_files(),
@@ -117,6 +30,90 @@ test_scenarios <- function(
   }
 }
 
+should_skip <- function(scenario_name, os = this_os()) {
+  scenarios <- testing_scenarios()
+  scenario_name <- match.arg(
+    arg = scenario_name,
+    choices = testing_scenario_names()
+  )
+  os %in% scenarios[scenario_name, ]$skip_os
+}
+
+set_testing_scenario <- function(scenario = NULL) {
+  scenario <- match.arg(scenario, choices = testing_scenario_names())
+  new <- list()
+  new[[test_option_name]] <- scenario
+  options(new)
+}
+
+set_test_backend <- function() {
+  eval(parse(text = get_testing_scenario()$backend))
+}
+
+get_testing_scenario <- function() {
+  scenario <- get_testing_scenario_name()
+  testing_scenarios()[scenario, ]
+}
+
+get_testing_scenario_name <- function() {
+  scenario <- getOption(test_option_name)
+  if (!length(scenario)) {
+    scenario <- match.arg(
+      arg = default_testing_scenario,
+      choices = testing_scenario_names()
+    )
+  }
+  scenario
+}
+
+testing_scenario_names <- function() {
+  rownames(testing_scenarios())
+}
+
+testing_scenarios <- function() {
+  file <- file.path("testing", "scenarios.csv")
+  path <- system.file(file, package = "drake", mustWork = TRUE)
+  x <- read.csv(path, stringsAsFactors = FALSE)
+  rn <- paste(
+    x$envir,
+    x$parallelism,
+    x$jobs,
+    x$backend,
+    x$caching,
+    sep = "_"
+  )
+  rn <- gsub(pattern = "_*$", replacement = "", x = rn)
+  rownames(x) <- rn
+  x$backend <- backend_code(x$backend)
+  x$envir <- envir_code(x$envir)
+  x$caching[!nzchar(x$caching)] <- "worker"
+  apply_skip_os(x)
+}
+
+# For the table of possible testing scenarios x.
+apply_skip_os <- function(x) {
+  x$skip_os <- ""
+  skip_on_windows <- grepl("mclapply|clustermq", x$parallelism)
+  x$skip_os[skip_on_windows] <- "windows"
+  x
+}
+
+backend_code <- function(x) {
+  ifelse(
+    nzchar(x),
+    paste0("future::plan(", x, ")"),
+    x
+  )
+}
+
+envir_code <- function(x) {
+  ifelse(
+    x == "local",
+    "new.env(parent = globalenv())",
+    "globalenv()"
+  )
+}
+
 unit_test_files <- function(path = getwd(), max_depth = 100) {
   # find the package root
   p <- normalizePath(dirname(path))
@@ -132,3 +129,6 @@ unit_test_files <- function(path = getwd(), max_depth = 100) {
   }
   stop("Maximum search of ", max_depth, " exceeded for ", path)
 }
+
+default_testing_scenario <- "local_loop_1"
+test_option_name <- "drake_test_scenario"
