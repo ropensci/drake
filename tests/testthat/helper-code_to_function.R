a06e1175a0c10db5c077eb44d1894e8a266eaa0a
thebioengineer <ellishughes@live.com>
2019-09-22 22:42:09 -0700

    de-lint based on {lintr}, and add tests for using RMD's as inputs

--- tests/testthat/helper-code_to_function.R
+++ tests/testthat/helper-code_to_function.R
@@ -3,15 +3,22 @@ setup_scripts <- function() {
   script2 <- tempfile()
   script3 <- tempfile()
 
-  tempDir <- normalizePath(tempdir(),winslash = "/")
+  tempDir <- normalizePath(tempdir(), winslash = "/")
   writeLines(c("data <- mtcars",
                "data$newcol <- 1",
-               paste0("saveRDS(data, \"",tempDir,"/testobject.rds\")")), script1)
-  writeLines(c(paste0("data <- readRDS(\"",tempDir,"/testobject.rds\")"),
+               paste0("saveRDS(data, \"",
+                      file.path(tempDir, "testobject.rds"), "\")")),
+             script1)
+  writeLines(c(paste0("data <- readRDS(\"",
+                      file.path(tempDir, "testobject.rds"), "\")"),
                "data$newcol2 <- 2",
-               paste0("saveRDS(data, \"",tempDir,"/testobject2.rds\")")), script2)
-  writeLines(c(paste0("data <- readRDS(\"",tempDir,"/testobject2.rds\")"),
-               "plot(data$newcol,data$newcol1)"), script3)
+               paste0("saveRDS(data, \"",
+                      file.path(tempDir, "testobject2.rds"), "\")")),
+             script2)
+  writeLines(c(paste0("data <- readRDS(\"",
+                      file.path(tempDir, "testobject2.rds"), "\")"),
+               "plot(data$newcol,data$newcol2)"),
+             script3)
 
   return(list(script1 = script1,
               script2 = script2,
@@ -19,26 +26,35 @@ setup_scripts <- function() {
               tempDir = tempDir))
 }
 
-update_script2_trivial <- function(script,tempDir) {
-  writeLines(c(paste0("data <- readRDS(\"",tempDir,"/testobject.rds\")"),
+update_script2_trivial <- function(script, tempDir) {
+  writeLines(c(paste0("data <- readRDS(\"",
+                      file.path(tempDir, "testobject.rds"), "\")"),
                "# trivial changes",
                "data$newcol2 <- 2",
-               paste0("saveRDS(data, \"",tempDir,"/testobject2.rds\")")), script)
+               paste0("saveRDS(data, \"",
+                      file.path(tempDir, "testobject2.rds"), "\")")),
+             script)
   code_to_function(script)
 }
 
-update_script2_non_trivial <- function(script,tempDir) {
-  writeLines(c(paste0("data <- readRDS(\"",tempDir,"/testobject.rds\")"),
+update_script2_non_trivial <- function(script, tempDir) {
+  writeLines(c(paste0("data <- readRDS(\"",
+                      file.path(tempDir, "testobject.rds"), "\")"),
                "data$newcol2 <- 3",
-               paste0("saveRDS(data, \"",tempDir,"/testobject2.rds\")")), script)
+               paste0("saveRDS(data, \"",
+                      file.path(tempDir, "testobject2.rds"), "\")")),
+             script)
   code_to_function(script)
 }
 
-update_script2_same_output <- function(script,tempDir) {
-  writeLines(c(paste0("data <- readRDS(\"",tempDir,"/testobject.rds\")"),
+update_script2_same_output <- function(script, tempDir) {
+  writeLines(c(paste0("data <- readRDS(\"",
+                      file.path(tempDir, "testobject.rds"), "\")"),
                "data$newcol2 <- 4",
                "data$newcol2 <- 3",
-               paste0("saveRDS(data, \"",tempDir,"/testobject2.rds\")")), script)
+               paste0("saveRDS(data, \"",
+                      file.path(tempDir, "testobject2.rds"), "\")")),
+             script)
   code_to_function(script)
 }
 
@@ -49,5 +65,155 @@ create_scripts_plan <- function() {
     step1 = script1_function(),
     step2 = script2_function(step1),
     step3 = script3_function(step2)
+  )
+}
+
+setup_rmd <- function() {
+  rmd1 <- tempfile()
+  rmd2 <- tempfile()
+  rmd3 <- tempfile()
+
+  tempDir <- normalizePath(tempdir(), winslash = "/")
+  writeLines(c(
+    "---",
+    "title: \"Test RMD 1\"",
+    "author: \"Unknown\"",
+    "output: html_document",
+    "---",
+    "This is a test rmd to load and save the `mtcars` dataset.",
+    "```{r load_data}",
+    "data <- mtcars",
+    "```",
+    "We will now add a new column!",
+    "```{r manipulate}",
+    "data$newcol <- 1",
+    "```",
+    "Now, we will save it!",
+    "```{r save_data}",
+    paste0("saveRDS(data, \"", file.path(tempDir, "testobject.rds"), "\")"),
+    "```"),
+    rmd1)
+  writeLines(c(
+      "---",
+      "title: \"Test RMD 2\"",
+      "author: \"Unknown\"",
+      "output: html_document",
+      "---",
+      "This is a test rmd to load a priorly edited `mtcars` dataset.",
+      "```{r load_data}",
+      paste0("data <- readRDS(\"", file.path(tempDir, "testobject.rds"), "\")"),
+      "```",
+      "We will now add a new column!",
+      "```{r manipulate}",
+      "data$newcol2 <- 2",
+      "```",
+      "Now, we will save it!",
+      "```{r save_data}",
+      paste0("saveRDS(data, \"", file.path(tempDir, "testobject2.rds"), "\")"),
+      "```"),
+      rmd2)
+  writeLines(c(
+    "---",
+    "title: \"Test RMD 3\"",
+    "author: \"Unknown\"",
+    "output: html_document",
+    "---",
+    "This is a test rmd to load and plot a priorly edited `mtcars` dataset.",
+    "```{r load_data}",
+    paste0("data <- readRDS(\"", file.path(tempDir, "testobject2.rds"), "\")"),
+    "```",
+    "We will now plot our data!",
+    "```{r plot}",
+    "plot(data$newcol,data$newcol2)",
+    "```"),
+    rmd3)
+
+  return(list(rmd1 = rmd1,
+              rmd2 = rmd2,
+              rmd3 = rmd3,
+              tempDir = tempDir))
+}
+
+update_rmd2_trivial <- function(rmd, tempDir) {
+  writeLines(c(
+    "---",
+    "title: \"Test RMD 2\"",
+    "author: \"Unknown\"",
+    "output: html_document",
+    "---",
+    "This is a test rmd to load a priorly edited `mtcars` dataset.",
+    "```{r load_data}",
+    paste0("data <- readRDS(\"", file.path(tempDir, "testobject.rds"), "\")"),
+    "```",
+    "We will now add a new column! I am adding a trivial change - a comment.",
+    "```{r manipulate}",
+    "# trivial changes",
+    "data$newcol2 <- 2",
+    "```",
+    "Now, we will save it!",
+    "```{r save_data}",
+    paste0("saveRDS(data, \"", file.path(tempDir, "testobject2.rds"), "\")"),
+    "```"),
+    rmd)
+  code_to_function(rmd)
+}
+
+update_rmd2_non_trivial <- function(rmd, tempDir) {
+  writeLines(c(
+    "---",
+    "title: \"Test RMD 2\"",
+    "author: \"Unknown\"",
+    "output: html_document",
+    "---",
+    "This is a test rmd to load a priorly edited `mtcars` dataset.",
+    "```{r load_data}",
+    paste0("data <- readRDS(\"", file.path(tempDir, "testobject.rds"), "\")"),
+    "```",
+    "We will now add a new column! I am adding a non trivial change -
+    a different number.",
+    "```{r manipulate}",
+    "data$newcol2 <- 3",
+    "```",
+    "Now, we will save it!",
+    "```{r save_data}",
+    paste0("saveRDS(data, \"", file.path(tempDir, "testobject2.rds"), "\")"),
+    "```"),
+    rmd)
+  code_to_function(rmd)
+}
+
+update_rmd2_same_output <- function(rmd, tempDir) {
+  writeLines(c(
+    "---",
+    "title: \"Test RMD 2\"",
+    "author: \"Unknown\"",
+    "output: html_document",
+    "---",
+    "This is a test rmd to load a priorly edited `mtcars` dataset.",
+    "```{r load_data}",
+    paste0("data <- readRDS(\"", file.path(tempDir, "testobject.rds"), "\")"),
+    "```",
+    "We will now add a new column! I am adding a non trivial change -
+    two manipulations that end up with the same output as before.",
+    "```{r manipulate}",
+    "data$newcol2 <- 4",
+    "data$newcol2 <- 3",
+    "```",
+    "Now, we will save it!",
+    "```{r save_data}",
+    paste0("saveRDS(data, \"", file.path(tempDir, "testobject2.rds"), "\")"),
+    "```"),
+    rmd)
+  code_to_function(rmd)
+
+}
+
+
+
+create_rmd_plan <- function() {
+  drake_plan(
+    step1 = rmd1_function(),
+    step2 = rmd2_function(step1),
+    step3 = rmd3_function(step2)
   )
 }
--- tests/testthat/test-code_to_function.R
+++ tests/testthat/test-code_to_function.R
@@ -2,7 +2,8 @@ drake_context("test-code_to_function")
 
 
 
-test_that("Scripts are evaluated into function that returns a time and file path", {
+test_that("Scripts are evaluated into function that returns a
+          time and file path", {
 
   script1 <- tempfile()
   script2 <- tempfile()
@@ -14,20 +15,22 @@ test_that("Scripts are evaluated into function that returns a time and file path
   function_results <- script1_function()
 
   expect_equal(function_contents[-length(function_contents)],
-               c("{",readLines(script1)))
-  expect_equal(length(function_results),2)
-  expect_s3_class(function_results$time,"POSIXct")
+               c("{", readLines(script1)))
+  expect_equal(length(function_results), 2)
+  expect_s3_class(function_results$time,
+                  "POSIXct")
 
 })
 
-test_that("Returned functions include only the scripts lines that need to be evaluated", {
+test_that("Returned functions include only the scripts lines that need
+          to be evaluated", {
 
   script1 <- tempfile()
   script2 <- tempfile()
 
-  script1_contents<-c("data <- mtcars",
+  script1_contents <- c("data <- mtcars",
                       "data$newcol <- 1")
-  script2_contents<-c("data <- mtcars",
+  script2_contents <- c("data <- mtcars",
                       "# Comment with un-necessary information",
                       "data$newcol <- 1")
 
@@ -41,64 +44,159 @@ test_that("Returned functions include only the scripts lines that need to be eva
   function2_contents <- as.character(body(script2_function))
 
   expect_equal(function1_contents[-length(function1_contents)],
-               c("{",readLines(script1)))
+               c("{", readLines(script1)))
   expect_equal(function1_contents,
                function2_contents)
   expect_false(isTRUE(all.equal(
     function2_contents[-length(function2_contents)],
-    c("{",readLines(script2)))))
+    c("{", readLines(script2)))))
 })
+
+
+test_that("Returned functions include only the scripts lines that need
+          to be evaluated from an RMD", {
+
+            rmd <- tempfile()
+
+            writeLines(c(
+              "---",
+              "title: \"Test RMD 1\"",
+              "author: \"Unknown\"",
+              "output: html_document",
+              "---",
+              "This is a test rmd to load and save the `mtcars` dataset.",
+              "```{r load_data}",
+              "data <- mtcars",
+              "```",
+              "We will now add a new column!",
+              "```{r manipulate}",
+              "data$newcol <- 1",
+              "```"),
+              rmd)
+
+            expected_contents <- c("data <- mtcars",
+                                   "data$newcol <- 1")
+
+            rmd_function <- code_to_function(rmd)
+            rmd_function_contents <- as.character(body(rmd_function))
+
+            expect_equal(rmd_function_contents[-length(rmd_function_contents)],
+                         c("{", expected_contents))
+})
+
 
 test_with_dir("drake tracks and updates for scripted functions", {
 
   #Setup scripts
   script_list <- setup_scripts()
-  script1_function<-code_to_function(script_list$script1)
-  script2_function<-code_to_function(script_list$script2)
-  script3_function<-code_to_function(script_list$script3)
+  script1_function <- code_to_function(script_list$script1)
+  script2_function <- code_to_function(script_list$script2)
+  script3_function <- code_to_function(script_list$script3)
 
   # create plan
   plan <- create_scripts_plan()
-  config<-drake_config(plan)
+  config <- drake_config(plan)
   make(plan)
   expect_equal(
     justbuilt(config),
-    c("step1","step2","step3")
+    c("step1", "step2", "step3")
   )
 
 
   # Change script2 trivially, there should be no updates
-  script2_function<-update_script2_trivial(script = script_list$script2,
+  script2_function <- update_script2_trivial(script = script_list$script2,
                                            tempDir = script_list$tempDir)
-  expect_equal(length(outdated(config)),0)
+  expect_equal(length(outdated(config)), 0)
 
 
   # change script2 non-trivially to cause update to all downstream targets.
-  script2_function<-update_script2_non_trivial(script = script_list$script2,
-                             tempDir = script_list$tempDir)
+  script2_function <- update_script2_non_trivial(script = script_list$script2,
+                              tempDir = script_list$tempDir)
 
   expect_equal(
     outdated(config),
-    c("step2","step3")
+    c("step2", "step3")
   )
   make(plan)
   expect_equal(
     justbuilt(config),
-    c("step2","step3")
+    c("step2", "step3")
   )
 
   # change script2 non-trivially but output would be the same.
   # behavior is like `gnumaker`, in that it will rerun all downstream functions
-  script2_function<-update_script2_same_output(script = script_list$script2,
+  script2_function <- update_script2_same_output(script = script_list$script2,
                                                tempDir = script_list$tempDir)
   expect_equal(
     outdated(config),
-    c("step2","step3")
+    c("step2", "step3")
   )
   make(plan)
   expect_equal(
     justbuilt(config),
-    c("step2","step3")
+    c("step2", "step3")
+  )
+
+  # nothing to do
+  finalOutput <- readd(step3)
+
+  make(plan)
+  expect_true(identical(finalOutput, readd(step3)))
+})
+
+
+
+test_with_dir("drake tracks and updates for Rmarkdown files that behave
+              like scripts", {
+
+  #Setup scripts
+  rmd_list <- setup_rmd()
+  rmd1_function <- code_to_function(rmd_list$rmd1)
+  rmd2_function <- code_to_function(rmd_list$rmd2)
+  rmd3_function <- code_to_function(rmd_list$rmd3)
+
+  # create plan
+  plan <- create_rmd_plan()
+  config <- drake_config(plan)
+  make(plan)
+  expect_equal(
+    justbuilt(config),
+    c("step1", "step2", "step3")
+  )
+
+
+  # Change rmd2 trivially, there should be no updates
+  rmd2_function <- update_rmd2_trivial(rmd = rmd_list$rmd2,
+                                       tempDir = rmd_list$tempDir)
+  expect_equal(length(outdated(config)), 0)
+
+
+  # change rmd2 non-trivially to cause update to all downstream targets.
+  rmd2_function <- update_rmd2_non_trivial(rmd = rmd_list$rmd2,
+                                           tempDir = rmd_list$tempDir)
+
+  expect_equal(
+    outdated(config),
+    c("step2", "step3")
+  )
+  make(plan)
+  expect_equal(
+    justbuilt(config),
+    c("step2", "step3")
+  )
+
+  # change rmd2 non-trivially but output would be the same.
+  # behavior is like `gnumaker`, in that it will rerun all downstream functions
+  rmd2_function <- update_rmd2_same_output(rmd = rmd_list$rmd2,
+                                           tempDir = rmd_list$tempDir)
+  expect_equal(
+    outdated(config),
+    c("step2", "step3")
+  )
+  make(plan)
+  expect_equal(
+    justbuilt(config),
+    c("step2", "step3")
   )
 
   # nothing to do
