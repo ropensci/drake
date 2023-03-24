c718d8a5c92967914547031642b717ef40b23e66
wlandau <will.landau@gmail.com>
2023-03-24 07:08:59 -0400

    Remove broken link

--- R/vis_drake_graph.R
+++ R/vis_drake_graph.R
@@ -4,7 +4,7 @@
 #' @description It is good practice to visualize the dependency graph
 #'   before running the targets.
 #' @details For enhanced interactivity in the graph, see the `mandrake`
-#'   package: <https://github.com/matthewstrasiotto/mandrake>.
+#'   package.
 #' @export
 #' @seealso [render_drake_graph()], [sankey_drake_graph()],
 #'   [drake_ggraph()], [text_drake_graph()]
@@ -145,7 +145,7 @@ body(vis_drake_graph) <- config_util_body(vis_drake_graph_impl)
 #' [vis_drake_graph()], which typical users
 #' call more often.
 #' @details For enhanced interactivity in the graph, see the `mandrake`
-#'   package: <https://github.com/matthewstrasiotto/mandrake>.
+#'   package.
 #' @export
 #' @seealso [vis_drake_graph()], [sankey_drake_graph()],
 #'   [drake_ggraph()]
